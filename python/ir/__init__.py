342deda59e5f776d3a7acbd40e82611ac00b01ca
Lin Jiang <jianglin332@outlook.com>
2021-02-25 20:09:16 +0800

    change filenames to avoid conflict

--- python/ir/__init__.py
+++ python/ir/__init__.py
@@ -1,9 +1,9 @@
 from ffi import AccessType, MemType, DataType
 from ffi import InvalidSchedule, InvalidProgram, DriverError
 
-from .ast import pop_ast, Var, VarDef, For, If, Else, MarkNid, NamedScope, Eval, Any
-from .ast import min, max, l_and, l_or, l_not, intrinsic
-from .ast_pass import *
+from .nodes import pop_ast, Var, VarDef, For, If, Else, MarkNid, NamedScope, Eval, Any
+from .nodes import min, max, l_and, l_or, l_not, intrinsic
+from .passes import *
 from .schedule import *
 from .codegen import codegen
 from .driver import *
