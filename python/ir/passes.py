fc11130b8caaada3bbfd4f31d3f7c5451ea0c310
yasarjames <3160536901@qq.com>
2021-03-07 18:27:50 +0800

    fix space

--- python/ir/passes.py
+++ python/ir/passes.py
@@ -17,30 +17,30 @@ from ffi import gpu_correct_shared
 from ffi import gpu_normalize_threads
 
 def lower(ast, target: Optional[ffi.Target]=None):
-	ast = simplify_pass(ast)
-	ast = remove_writes(ast)
-	ast = sink_var(ast)
-	ast = shrink_var(ast)
-	ast = shrink_for(ast)
-	ast = merge_if(ast)
-	ast = seperate_tail(ast)
-	ast = make_atomic(ast)
+    ast = simplify_pass(ast)
+    ast = remove_writes(ast)
+    ast = sink_var(ast)
+    ast = shrink_var(ast)
+    ast = shrink_for(ast)
+    ast = merge_if(ast)
+    ast = seperate_tail(ast)
+    ast = make_atomic(ast)
 
-	# After all general analysis
-	ast = use_builtin_div(ast)
+    # After all general analysis
+    ast = use_builtin_div(ast)
 
-	if target is None:
-		return ast
+    if target is None:
+        return ast
 
-	if target.type() == ffi.TargetType.GPU:
-		ast = gpu_make_sync(ast)
+    if target.type() == ffi.TargetType.GPU:
+        ast = gpu_make_sync(ast)
 
-		# No more shrink_var after this pass
-		ast = gpu_correct_shared(ast)
+        # No more shrink_var after this pass
+        ast = gpu_correct_shared(ast)
 
-		# After gpu_make_sync and gpu_correct_shared. Otherwise, these 2 passes
-		# cannot get the right thread info
-		ast = gpu_normalize_threads(ast)
+        # After gpu_make_sync and gpu_correct_shared. Otherwise, these 2 passes
+        # cannot get the right thread info
+        ast = gpu_normalize_threads(ast)
 
-	return ast
+    return ast
 
