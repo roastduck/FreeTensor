a10559bde03c88f76a87de99269766fca4037ce7
yasarjames <3160536901@qq.com>
2021-03-07 18:22:57 +0800

    fix space

--- test/pass/test_sink_var.py
+++ test/pass/test_sink_var.py
@@ -1,53 +1,53 @@
 import ir
 
 def test_sink_stmt_seq():
-	with ir.VarDef([
-			("x", (5,), "int32", "input", "cpu"),
-			("y", (4,), "int32", "output", "cpu")]) as (x, y):
-		with ir.VarDef("b", (1,), "int32", "cache", "cpu") as b:
-			with ir.For("i", 0, 4) as i:
-				b[0] = x[i] + x[i + 1]
-				y[i] = b[0] * i
-			y[0] = 0
-	ast = ir.pop_ast()
-	print(ast)
-	ast = ir.lower(ast)
-	print(ast)
+    with ir.VarDef([
+            ("x", (5,), "int32", "input", "cpu"),
+            ("y", (4,), "int32", "output", "cpu")]) as (x, y):
+        with ir.VarDef("b", (1,), "int32", "cache", "cpu") as b:
+            with ir.For("i", 0, 4) as i:
+                b[0] = x[i] + x[i + 1]
+                y[i] = b[0] * i
+            y[0] = 0
+    ast = ir.pop_ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
 
-	with ir.VarDef([
-			("x", (5,), "int32", "input", "cpu"),
-			("y", (4,), "int32", "output", "cpu")]) as (x, y):
-		with ir.VarDef("b", (1,), "int32", "cache", "cpu") as b:
-			with ir.For("i", 0, 4) as i:
-				b[0] = x[i] + x[i + 1]
-				y[i] = b[0] * i
-		y[0] = 0
-	std = ir.pop_ast()
+    with ir.VarDef([
+            ("x", (5,), "int32", "input", "cpu"),
+            ("y", (4,), "int32", "output", "cpu")]) as (x, y):
+        with ir.VarDef("b", (1,), "int32", "cache", "cpu") as b:
+            with ir.For("i", 0, 4) as i:
+                b[0] = x[i] + x[i + 1]
+                y[i] = b[0] * i
+        y[0] = 0
+    std = ir.pop_ast()
 
-	assert std.match(ast)
+    assert std.match(ast)
 
 def test_sink_for():
-	with ir.VarDef([
-			("x", (5,), "int32", "input", "cpu"),
-			("y", (4,), "int32", "output", "cpu")]) as (x, y):
-		with ir.VarDef("b", (4,), "int32", "cache", "cpu") as b:
-			with ir.For("i", 0, 4) as i:
-				b[i] = x[i] + x[i + 1]
-				y[i] = b[i] * i
-	ast = ir.pop_ast()
-	print(ast)
-	ast = ir.lower(ast)
-	print(ast)
+    with ir.VarDef([
+            ("x", (5,), "int32", "input", "cpu"),
+            ("y", (4,), "int32", "output", "cpu")]) as (x, y):
+        with ir.VarDef("b", (4,), "int32", "cache", "cpu") as b:
+            with ir.For("i", 0, 4) as i:
+                b[i] = x[i] + x[i + 1]
+                y[i] = b[i] * i
+    ast = ir.pop_ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
 
-	with ir.VarDef([
-			("x", (5,), "int32", "input", "cpu"),
-			("y", (4,), "int32", "output", "cpu")]) as (x, y):
-		with ir.For("i", 0, 4) as i:
-			# Also shrinked
-			with ir.VarDef("b", (1,), "int32", "cache", "cpu") as b:
-				b[0] = x[i] + x[i + 1]
-				y[i] = b[0] * i
-	std = ir.pop_ast()
+    with ir.VarDef([
+            ("x", (5,), "int32", "input", "cpu"),
+            ("y", (4,), "int32", "output", "cpu")]) as (x, y):
+        with ir.For("i", 0, 4) as i:
+            # Also shrinked
+            with ir.VarDef("b", (1,), "int32", "cache", "cpu") as b:
+                b[0] = x[i] + x[i + 1]
+                y[i] = b[0] * i
+    std = ir.pop_ast()
 
-	assert std.match(ast)
+    assert std.match(ast)
 
--- test/schedule/test_split.py
+++ test/schedule/test_split.py
@@ -2,80 +2,80 @@ import ir
 import pytest
 
 def test_factor():
-	with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
-		with ir.For("i", 0, 8, nid="L1") as i:
-			y[i] = i
-	ast = ir.pop_ast()
-	print(ast)
-	s = ir.Schedule(ast)
-	s.split("L1", 4)
-	ast = s.ast()
-	print(ast)
-	ast = ir.lower(ast)
-	print(ast)
+    with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
+        with ir.For("i", 0, 8, nid="L1") as i:
+            y[i] = i
+    ast = ir.pop_ast()
+    print(ast)
+    s = ir.Schedule(ast)
+    s.split("L1", 4)
+    ast = s.ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
 
-	with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
-		with ir.For("i.0", 0, 2) as i0:
-			with ir.For("i.1", 0, 4) as i1:
-				y[4 * i0 + i1] = 4 * i0 + i1
-	std = ir.pop_ast()
+    with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
+        with ir.For("i.0", 0, 2) as i0:
+            with ir.For("i.1", 0, 4) as i1:
+                y[4 * i0 + i1] = 4 * i0 + i1
+    std = ir.pop_ast()
 
-	assert std.match(ast)
+    assert std.match(ast)
 
 def test_nparts():
-	with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
-		with ir.For("i", 0, 8, nid="L1") as i:
-			y[i] = i
-	ast = ir.pop_ast()
-	print(ast)
-	s = ir.Schedule(ast)
-	s.split("L1", nparts=4)
-	ast = s.ast()
-	print(ast)
-	ast = ir.lower(ast)
-	print(ast)
+    with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
+        with ir.For("i", 0, 8, nid="L1") as i:
+            y[i] = i
+    ast = ir.pop_ast()
+    print(ast)
+    s = ir.Schedule(ast)
+    s.split("L1", nparts=4)
+    ast = s.ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
 
-	with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
-		with ir.For("i.0", 0, 4) as i0:
-			with ir.For("i.1", 0, 2) as i1:
-				y[2 * i0 + i1] = 2 * i0 + i1
-	std = ir.pop_ast()
+    with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
+        with ir.For("i.0", 0, 4) as i0:
+            with ir.For("i.1", 0, 2) as i1:
+                y[2 * i0 + i1] = 2 * i0 + i1
+    std = ir.pop_ast()
 
-	assert std.match(ast)
+    assert std.match(ast)
 
 def test_guard():
-	with ir.VarDef("y", (10,), "int32", "output", "cpu") as y:
-		with ir.For("i", 0, 10, nid="L1") as i:
-			y[i] = i
-	ast = ir.pop_ast()
-	print(ast)
-	s = ir.Schedule(ast)
-	s.split("L1", 4)
-	ast = s.ast()
-	print(ast)
-	ast = ir.lower(ast)
-	print(ast)
+    with ir.VarDef("y", (10,), "int32", "output", "cpu") as y:
+        with ir.For("i", 0, 10, nid="L1") as i:
+            y[i] = i
+    ast = ir.pop_ast()
+    print(ast)
+    s = ir.Schedule(ast)
+    s.split("L1", 4)
+    ast = s.ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
 
-	# The result after the seperate_tail pass
-	with ir.VarDef("y", (10,), "int32", "output", "cpu") as y:
-		with ir.For("i.0", 0, 2) as i0:
-			with ir.For("i.1", 0, 4) as i1:
-				y[4 * i0 + i1] = 4 * i0 + i1
-		with ir.For("i.1", 0, 2) as i1:
-			y[8 + i1] = 8 + i1
-	std = ir.pop_ast()
+    # The result after the seperate_tail pass
+    with ir.VarDef("y", (10,), "int32", "output", "cpu") as y:
+        with ir.For("i.0", 0, 2) as i0:
+            with ir.For("i.1", 0, 4) as i1:
+                y[4 * i0 + i1] = 4 * i0 + i1
+        with ir.For("i.1", 0, 2) as i1:
+            y[8 + i1] = 8 + i1
+    std = ir.pop_ast()
 
-	assert std.match(ast)
+    assert std.match(ast)
 
 def test_not_found():
-	with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
-		with ir.For("i", 0, 8) as i:
-			y[i] = i
-	ast = ir.pop_ast()
-	print(ast)
-	s = ir.Schedule(ast)
-	with pytest.raises(ir.InvalidSchedule):
-		s.split("L1", 4)
-	ast_ = s.ast() # Should not changed
-	assert ast_.match(ast)
+    with ir.VarDef("y", (8,), "int32", "output", "cpu") as y:
+        with ir.For("i", 0, 8) as i:
+            y[i] = i
+    ast = ir.pop_ast()
+    print(ast)
+    s = ir.Schedule(ast)
+    with pytest.raises(ir.InvalidSchedule):
+        s.split("L1", 4)
+    ast_ = s.ast() # Should not changed
+    assert ast_.match(ast)
 
