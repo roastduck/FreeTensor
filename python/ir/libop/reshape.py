5b49d768989ea22f1140e146f93dffd3e13fb75e
Shizhi Tang <rd0x01@gmail.com>
2021-06-13 14:28:19 +0800

    libop/unsqueeze

--- python/ir/libop/reshape.py
+++ python/ir/libop/reshape.py
@@ -1,3 +1,5 @@
+from typing import Sequence, Optional
+
 from .. import core
 from .common import StaticType as StaticType
 
@@ -100,3 +102,104 @@ def flatten(t_x: StaticType,
         return y_shape, y
 
     return f_flatten
+
+
+def unsqueeze_(t_x: StaticType,
+               t_y: StaticType,
+               io_mem,
+               idx_dtype="int32",
+               axes: Optional[Sequence[int]] = None):
+
+    # ONNX >= 13 treats axes as a tensor, which we don't support for now
+    assert axes is not None, "Currently only unsqueeze for ONNX < 13 is supported"
+    axes = sorted(map(lambda x: x if x >= 0 else t_y.ndim + x, axes))
+
+    def begin_with_0(lst):
+        return len(lst) > 0 and lst[0] == 0
+
+    def all_minus_one(lst):
+        return list(map(lambda x: x - 1, lst))
+
+    @core.transform
+    def f_unsqueeze(x_shape, y_shape, x, y):
+        'nid: V_x_shape'
+        core.declare_var(x_shape, (t_x.ndim,), idx_dtype, "input", io_mem)
+        'nid: V_y_shape'
+        core.declare_var(y_shape, (t_y.ndim,), idx_dtype, "input", io_mem)
+        'nid: V_x'
+        core.declare_var(x, x_shape, t_x.elem_type, "input", io_mem)
+        'nid: V_y'
+        core.declare_var(y, y_shape, t_x.elem_type, "output", io_mem)
+
+        if t_y.ndim == 0:
+            y[()] = x[()]
+        elif begin_with_0(axes):
+            'nid: recur'
+            unsqueeze_(t_x, t_y.one_less_dim(), io_mem, idx_dtype,
+                       all_minus_one(axes[1:]))(x_shape, y_shape[1:], x, y[0])
+        else:
+            'nid: L'
+            for i in range(x_shape[0]):
+                'nid: recur'
+                unsqueeze_(t_x.one_less_dim(),
+                           t_y.one_less_dim(), io_mem, idx_dtype,
+                           all_minus_one(axes))(x_shape[1:], y_shape[1:], x[i],
+                                                y[i])
+
+    return f_unsqueeze
+
+
+def unsqueeze(t_x: StaticType,
+              t_y: StaticType,
+              io_mem,
+              idx_dtype="int32",
+              axes: Optional[Sequence[int]] = None):
+
+    # ONNX >= 13 treats axes as a tensor, which we don't support for now
+    assert axes is not None, "Currently only unsqueeze for ONNX < 13 is supported"
+    axes = sorted(map(lambda x: x if x >= 0 else t_y.ndim + x, axes))
+
+    def begin_with_0(lst):
+        return len(lst) > 0 and lst[0] == 0
+
+    def all_minus_one(lst):
+        return list(map(lambda x: x - 1, lst))
+
+    def comp_shape(t_x: StaticType, t_y: StaticType, axes):
+
+        @core.transform
+        def f_shape(x_shape, y_shape):
+            'nid: V_x_shape'
+            core.declare_var(x_shape, (t_x.ndim,), idx_dtype, "input", io_mem)
+            'nid: V_y_shape'
+            core.declare_var(y_shape, (t_y.ndim,), idx_dtype, "output", io_mem)
+
+            if t_y.ndim > 0:
+                if begin_with_0(axes):
+                    y_shape[0] = 1
+                    comp_shape(t_x, t_y.one_less_dim(),
+                               all_minus_one(axes[1:]))(x_shape, y_shape[1:])
+                else:
+                    y_shape[0] = x_shape[0]
+                    comp_shape(t_x.one_less_dim(), t_y.one_less_dim(),
+                               all_minus_one(axes))(x_shape[1:], y_shape[1:])
+
+        return f_shape
+
+    @core.transform
+    def f_unsqueeze(x_shape, x):
+        'nid: V_x_shape'
+        core.declare_var(x_shape, (t_x.ndim,), idx_dtype, "input", io_mem)
+        'nid: V_x'
+        core.declare_var(x, x_shape, t_x.elem_type, "input", io_mem)
+        'nid: V_y_shape'
+        y_shape = core.create_var((t_y.ndim,), idx_dtype, "output", io_mem)
+        'nid: shape'
+        comp_shape(t_x, t_y, axes)(x_shape, y_shape)
+        'nid: V_y'
+        y = core.create_var(y_shape, t_x.elem_type, "output", io_mem)
+        'nid: recur'
+        unsqueeze_(t_x, t_y, io_mem, idx_dtype, axes)(x_shape, y_shape, x, y)
+        return y_shape, y
+
+    return f_unsqueeze
