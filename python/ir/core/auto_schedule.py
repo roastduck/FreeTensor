b297e570c3771f2c299bddca8bf001c88cfe01f0
Yasar <3160536901@qq.com>
2021-08-14 13:25:39 +0800

    Add exploration-exploitation (#19)

--- python/ir/core/auto_schedule.py
+++ python/ir/core/auto_schedule.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class AutoSchedule(ffi.AutoSchedule):
+
     def set_params(self, *args, **kws):
         super(AutoSchedule, self).set_params(args, kws)
 
@@ -14,8 +15,10 @@ class AutoSchedule(ffi.AutoSchedule):
         dtrain = xgb.DMatrix(annotations, times)
         params = {}
         booster = xgb.train(params, dtrain)
+        block = iteration // 20
         for i in range(iteration):
-            print("iteration {}".format(i))
+            rand_num = max(0, 16 - i // block)
+            print("iteration {} rand {}".format(i, rand_num))
             sketches = super(AutoSchedule, self).get_random_sketches(100)
             annotations = []
             for sketch in sketches:
@@ -23,10 +26,11 @@ class AutoSchedule(ffi.AutoSchedule):
             annotations = np.array(annotations)
             annotations = xgb.DMatrix(annotations)
             pred = booster.predict(annotations)
-            sketch_preds = list(zip(pred, sketches))
-            sketch_preds.sort(key=lambda x:x[0])
-            sketches = list(zip(*sketch_preds))[1][:20]
-            annotations, times = super(AutoSchedule, self).test_and_add(sketches)
+            sketch_preds = list(zip(pred, sketches))[rand_num:]
+            sketch_preds.sort(key=lambda x: x[0])
+            sketches[rand_num:] = list(zip(*sketch_preds))[1][:20 - rand_num]
+            annotations, times = super(AutoSchedule,
+                                       self).test_and_add(sketches)
             annotations = np.array(annotations)
             times = np.array(times)
             dtrain = xgb.DMatrix(annotations, times)
