748292d2f2c60f9fbf4e81a22c67be19c3f1c364
Shizhi Tang <rd0x01@gmail.com>
2021-08-16 15:25:19 +0800

    fixed-lengthed feature

--- python/ir/core/analyze.py
+++ python/ir/core/analyze.py
@@ -1,4 +1,5 @@
 import ffi
 
 from ffi import structural_feature
+from ffi import fixed_length_feature
 from ffi import find_multi_level_tiling
---
+++ test/1.analyze/test_fixed_length_feature.py
@@ -0,0 +1,32 @@
+import ir
+
+STANDALONE_FEATURES = 0
+SAMPLE_GROUPS = 10
+SAMPLE_ITERS = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
+SAMPLE_FEATURES = 26
+FEAT_SAMP_CPU_LOAD_AREA = 14
+
+
+def test_sample_basic():
+    with ir.VarDef([
+        ("x", (128,), "float32", "input", "cpu"),
+        ("y", (128, 32), "float32", "output", "cpu"),
+    ]) as (x, y):
+        with ir.For("i", 0, 4) as i:
+            with ir.For("j", 0, 32) as j:
+                with ir.For("k", 0, 32) as k:
+                    y[i * 32 + k, j] = x[i * 32 + k] * 2
+    ast = ir.pop_ast()
+    print(ast)
+
+    features = ir.fixed_length_feature(ast)
+
+    assert features[STANDALONE_FEATURES +
+                    SAMPLE_ITERS.index(32) * SAMPLE_FEATURES +
+                    FEAT_SAMP_CPU_LOAD_AREA] == 32  # Load 32 every 32 iters
+    assert features[
+        STANDALONE_FEATURES + SAMPLE_ITERS.index(1024) * SAMPLE_FEATURES +
+        FEAT_SAMP_CPU_LOAD_AREA] == 32  # Load 32 every 32 * 32 iters
+    assert features[STANDALONE_FEATURES +
+                    SAMPLE_ITERS.index(4096) * SAMPLE_FEATURES +
+                    FEAT_SAMP_CPU_LOAD_AREA] == 128  # Load 128 in total
