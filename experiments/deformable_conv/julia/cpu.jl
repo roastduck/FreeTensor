32ab4e304f4171b7c82e13e1b7d2b663da7fead6
zhyuan020-gorgon <zhyuan020@qq.com>
2021-10-02 23:49:14 +0800

    Optimize experiments/deformable_conv/julia & experiments/longformer/julia

--- experiments/deformable_conv/julia/cpu.jl
+++ experiments/deformable_conv/julia/cpu.jl
@@ -1,58 +1,58 @@
 function conv(X, W1, W2, Y, n, c_in, c_out, h, w, k_h, k_w)
-    Threads.@threads for i = 1:n
-        for p = 1:h
-            for q = 1:w
-                row = zeros(Float32, (k_h, k_w))
-                col = zeros(Float32, (k_h, k_w))
-                row_int = zeros(Int, (k_h, k_w))
-                col_int = zeros(Int, (k_h, k_w))
+    # for i,p,q = 1:n,h,w
+    Threads.@threads for ipq = 0:n*h*w-1
+        i = div(ipq, h*w) + 1
+        p = div(mod(ipq, h*w), w) + 1
+        q = mod(ipq, w) + 1
+        row = zeros(Float32, (k_w, k_h))
+        col = zeros(Float32, (k_w, k_h))
+        row_int = zeros(Int, (k_w, k_h))
+        col_int = zeros(Int, (k_w, k_h))
+        for ro = 1:k_h
+            for so = 1:k_w
+                for ki = 1:c_in
+                    for ri = 0:k_h - 1
+                        for si = 0:k_w - 1
+                            if p + ri >= 1 && p + ri <= h && q + si >= 1 && q + si <= w
+                                row[so, ro] += X[q + si, p + ri, ki, i] * W1[si + 1, ri + 1, ki, 1, so, ro]
+                                col[so, ro] += X[q + si, p + ri, ki, i] * W1[si + 1, ri + 1, ki, 2, so, ro]
+                            end
+                        end
+                    end
+                end
+                row[so, ro] /= c_in
+                col[so, ro] /= c_in
+                row_int[so, ro] = trunc(Int, row[so, ro])
+                col_int[so, ro] = trunc(Int, col[so, ro])
+            end
+        end
+        pixel = zeros(Float32, (k_w, k_h, c_in))
+        for ki = 1:c_in
+            for ro = 1:k_h
+                for so = 1:k_w
+                    x = p + ro + row_int[so, ro]
+                    y = q + so + col_int[so, ro]
+                    if x >= 1 && x <= h && y >= 1 && y <= w
+                        pixel[so, ro, ki] += X[y, x, ki, i] * (row[so, ro] - row_int[so, ro]) * (col[so, ro] - col_int[so, ro])
+                    end
+                    if x >= 1 && x <= h && y + 1 >= 1 && y + 1 <= w
+                        pixel[so, ro, ki] += X[y + 1, x, ki, i] * (row[so, ro] - row_int[so, ro]) * (col_int[so, ro] + 1 - col[so, ro])
+                    end
+                    if x + 1 >= 1 && x + 1 <= h && y >= 1 && y <= w
+                        pixel[so, ro, ki] += X[y, x + 1, ki, i] * (row_int[so, ro] + 1 - row[so, ro]) * (col[so, ro] - col_int[so, ro])
+                    end
+                    if x + 1 >= 1 && x + 1 <= h && y + 1 >= 1 && y + 1 <= w
+                        pixel[so, ro, ki] += X[y + 1, x + 1, ki, i] * (row_int[so, ro] + 1 - row[so, ro]) * (col_int[so, ro] + 1 - col[so, ro])
+                    end
+                end
+            end
+        end
+        for ko = 1:c_out
+            Y[q, p, ko, i] = 0
+            for ki = 1:c_in
                 for ro = 1:k_h
                     for so = 1:k_w
-                        for ki = 1:c_in
-                            for ri = 0:k_h - 1
-                                for si = 0:k_w - 1
-                                    if p + ri >= 1 && p + ri <= h && q + si >= 1 && q + si <= w
-                                        row[ro, so] += X[i, ki, p + ri, q + si] * W1[ro, so, 1, ki, ri + 1, si + 1]
-                                        col[ro, so] += X[i, ki, p + ri, q + si] * W1[ro, so, 2, ki, ri + 1, si + 1]
-                                    end
-                                end
-                            end
-                        end
-                        row[ro, so] /= c_in
-                        col[ro, so] /= c_in
-                        row_int[ro, so] = trunc(Int, row[ro, so])
-                        col_int[ro, so] = trunc(Int, col[ro, so])
-                    end
-                end
-                pixel = zeros(Float32, (c_in, k_h, k_w))
-                for ki = 1:c_in
-                    for ro = 1:k_h
-                        for so = 1:k_w
-                            x = p + ro + row_int[ro, so]
-                            y = q + so + col_int[ro, so]
-                            if x >= 1 && x <= h && y >= 1 && y <= w
-                                pixel[ki, ro, so] += X[i, ki, x, y] * (row[ro, so] - row_int[ro, so]) * (col[ro, so] - col_int[ro, so])
-                            end
-                            if x >= 1 && x <= h && y + 1 >= 1 && y + 1 <= w
-                                pixel[ki, ro, so] += X[i, ki, x, y + 1] * (row[ro, so] - row_int[ro, so]) * (col_int[ro, so] + 1 - col[ro, so])
-                            end
-                            if x + 1 >= 1 && x + 1 <= h && y >= 1 && y <= w
-                                pixel[ki, ro, so] += X[i, ki, x + 1, y] * (row_int[ro, so] + 1 - row[ro, so]) * (col[ro, so] - col_int[ro, so])
-                            end
-                            if x + 1 >= 1 && x + 1 <= h && y + 1 >= 1 && y + 1 <= w
-                                pixel[ki, ro, so] += X[i, ki, x + 1, y + 1] * (row_int[ro, so] + 1 - row[ro, so]) * (col_int[ro, so] + 1 - col[ro, so])
-                            end
-                        end
-                    end
-                end
-                for ko = 1:c_out
-                    Y[i, ko, p, q] = 0
-                    for ki = 1:c_in
-                        for ro = 1:k_h
-                            for so = 1:k_w
-                                Y[i, ko, p, q] += pixel[ki, ro, so] * W2[ko, ki, ro, so]
-                            end
-                        end
+                        Y[q, p, ko, i] += pixel[so, ro, ki] * W2[so, ro, ki, ko]
                     end
                 end
             end
@@ -68,12 +68,12 @@ h = 56
 w = 56
 k_h = 3
 k_w = 3
-x = map(x -> x * 2 - 1, rand(Float32, (n, c_in, h, w)))
-w1 = map(x -> x * 2 - 1, rand(Float32, (k_h, k_w, 2, c_in, k_h, k_w)))
-w2 = map(x -> x * 2 - 1, rand(Float32, (c_out, c_in, k_h, k_w)))
-y = zeros(Float32, (n, c_out, h, w))
+x = map(x -> x * 2 - 1, rand(Float32, (w, h, c_in, n)))
+w1 = map(x -> x * 2 - 1, rand(Float32, (k_w, k_h, c_in, 2, k_w, k_h)))
+w2 = map(x -> x * 2 - 1, rand(Float32, (k_w, k_h, c_in, c_out)))
+y = zeros(Float32, (w, h, c_out, n))
 
-test_num = 1000
+test_num = 100
 conv(x, w1, w2, y, n, c_in, c_out, h, w, k_h, k_w)
 time = @timed begin
     for i = 1:test_num
