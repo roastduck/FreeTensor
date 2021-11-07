18a661f7e995afca41d6cfc5ad5570cae1c92c06
zhyuan020 <61464140+zhyuan020@users.noreply.github.com>
2021-11-07 21:32:45 +0800

    Optimize experiments/deformable_conv/julia & experiments/longformer/j… (#24)
    * Optimize experiments/deformable_conv/julia & experiments/longformer/julia
    
    * optimize experiments/subdivnet/julia
    
    * optimize experiments/softrast/julia

--- experiments/longformer/julia/cpu.jl
+++ experiments/longformer/julia/cpu.jl
@@ -1,57 +1,58 @@
 function transformer(Q, K, V, Y, w, dilation, dilation_heads, n_heads, seq_len, feat_len)
     sqrt_d = sqrt(feat_len)
-    Threads.@threads for i = 1:n_heads
-        for j = 1:seq_len
-            dot = zeros(Float32, 2 * w + 1)
-            for k = (-w):w
-                if i <= dilation_heads
-                    if j + k >= 1 && j + k <= seq_len
-                        for p = 1:feat_len
-                            dot[k + w + 1] += Q[i, j, p] * K[i, j + k, p]
-                        end
-                    end
-                else
-                    if j + k * dilation >= 1 && j + k * dilation <= seq_len
-                        for p = 1:feat_len
-                            dot[k + w + 1] += Q[i, j, p] * K[i, j + k * dilation, p]
-                        end
-                    end
-                end
-            end
-            maxval = -Inf
-            for k = 1:2 * w + 1
-                maxval = max(maxval, dot[k])
-            end
-            expval = zeros(Float32, 2 * w + 1)
-            for k = 1:2 * w + 1
-                expval[k] = exp(dot[k] - maxval)
-            end
-            expsum = 0
-            for k = 1:2 * w + 1
-                expsum += expval[k]
-            end
-            attn = zeros(Float32, 2 * w + 1)
-            for k = 1:2 * w + 1
-                attn[k] = expval[k] / expsum / sqrt_d
-            end
-
-            for p = 1:feat_len
-                Y[i, j, p] = 0
-            end
+    # for i, j = 1: n_heads, seq_len
+    Threads.@threads for ij = 0:n_heads*seq_len-1
+        i = div(ij, seq_len) + 1
+        j = mod(ij, seq_len) + 1
+        dot = zeros(Float32, 2 * w + 1)
+        for k = (-w):w
             if i <= dilation_heads
-                for k = (-w):w
-                    if j + k >= 1 && j + k <= seq_len
-                        for p = 1:feat_len
-                            Y[i, j, p] += attn[k + w + 1] * V[i, j + k, p]
-                        end
+                if j + k >= 1 && j + k <= seq_len
+                    for p = 1:feat_len
+                        dot[k + w + 1] += Q[p, j, i] * K[p, j + k, i]
                     end
                 end
             else
-                for k = (-w):w
-                    if j + k * dilation >= 1 && j + k * dilation <= seq_len
-                        for p = 1:feat_len
-                            Y[i, j, p] += attn[k + w + 1] * V[i, j + k * dilation, p]
-                        end
+                if j + k * dilation >= 1 && j + k * dilation <= seq_len
+                    for p = 1:feat_len
+                        dot[k + w + 1] += Q[p, j, i] * K[p, j + k * dilation, i]
+                    end
+                end
+            end
+        end
+        maxval = -Inf
+        for k = 1:2 * w + 1
+            maxval = max(maxval, dot[k])
+        end
+        expval = zeros(Float32, 2 * w + 1)
+        for k = 1:2 * w + 1
+            expval[k] = exp(dot[k] - maxval)
+        end
+        expsum = 0
+        for k = 1:2 * w + 1
+            expsum += expval[k]
+        end
+        attn = zeros(Float32, 2 * w + 1)
+        for k = 1:2 * w + 1
+            attn[k] = expval[k] / expsum / sqrt_d
+        end
+
+        for p = 1:feat_len
+            Y[p, j, i] = 0
+        end
+        if i <= dilation_heads
+            for k = (-w):w
+                if j + k >= 1 && j + k <= seq_len
+                    for p = 1:feat_len
+                        Y[p, j, i] += attn[k + w + 1] * V[p, j + k, i]
+                    end
+                end
+            end
+        else
+            for k = (-w):w
+                if j + k * dilation >= 1 && j + k * dilation <= seq_len
+                    for p = 1:feat_len
+                        Y[p, j, i] += attn[k + w + 1] * V[p, j + k * dilation, i]
                     end
                 end
             end
@@ -65,12 +66,12 @@ feat_len = 512
 w = 32
 dilation = 4  # counts from 1
 dilation_heads = 2
-q = rand(Float32, (n_heads, seq_len, feat_len))
-k = rand(Float32, (n_heads, seq_len, feat_len))
-v = rand(Float32, (n_heads, seq_len, feat_len))
-y = zeros(Float32, (n_heads, seq_len, feat_len))
+q = rand(Float32, (feat_len, seq_len, n_heads))
+k = rand(Float32, (feat_len, seq_len, n_heads))
+v = rand(Float32, (feat_len, seq_len, n_heads))
+y = zeros(Float32, (feat_len, seq_len, n_heads))
 
-test_num = 1000
+test_num = 5
 transformer(q, k, v, y, w, dilation, dilation_heads, n_heads, seq_len, feat_len)
 time = @timed begin
     for i = 1:test_num
