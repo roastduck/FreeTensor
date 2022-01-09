1e9a15dd364414fc947ed28dcfc9c5829251fe62
Blealtan Cao <blealtan@outlook.com>
2022-01-09 17:00:20 +0800

    Export Staged* from new_transformer.

--- python/ir/core/__init__.py
+++ python/ir/core/__init__.py
@@ -48,7 +48,7 @@ from .codegen import codegen
 from .driver import *
 from .config import *
 
-from .new_transformer import transform, inline, create_var, declare_var, capture_var, StagingError
+from .new_transformer import transform, inline, create_var, declare_var, capture_var, StagingError, StagedAssignable, StagedCallable, StagedIterable
 
 from .meta import *
 from .auto_schedule import *
