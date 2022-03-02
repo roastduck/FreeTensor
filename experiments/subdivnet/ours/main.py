e30eec7fc71cb9df8a4513ce43e3dafbeff500fa
Shizhi Tang <rd0x01@gmail.com>
2022-03-02 16:52:31 +0800

    Report inference compiling time in experiment scripts

--- experiments/subdivnet/ours/main.py
+++ experiments/subdivnet/ours/main.py
@@ -43,13 +43,16 @@ def compile_all(n_faces, in_feats, out_feats, device):
 
     print("# Inference:")
     print(inference)
+    t0 = time.time()
     s = ir.Schedule(inference)
     s.auto_schedule(device.target())
     f = ir.lower(s.func(), device.target())
-    print(f)
     code = ir.codegen(f, device.target())
-    print(ir.debug.with_line_no(code))
     inference_exe = ir.Driver(inference, code, device)
+    t1 = time.time()
+    print(f)
+    print(ir.debug.with_line_no(code))
+    print(f"Inference compiling time: {t1 - t0}s")
 
     print("# Forward:")
     print(forward)
