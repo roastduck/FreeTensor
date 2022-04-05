60b65c9714b42cbb1ec652bf949540a11cf7b7a3
Shizhi Tang <rd0x01@gmail.com>
2022-04-05 16:30:27 +0800

    3.schedule/test_fuse.py::test_fuse_with_if

--- test/3.schedule/test_fuse.py
+++ test/3.schedule/test_fuse.py
@@ -400,3 +400,39 @@ def test_fuse_no_deps_3():
         s.fuse("Li1", "Li2")
     ast = s.ast()  # Should not changed
     assert ast.match(test.body)
+
+
+def test_fuse_with_if():
+    with ir.VarDef([
+        ("c", (), "int32", "input", "cpu"),
+        ("y", (4, 8), "int32", "output", "cpu"),
+        ("z", (4, 8), "int32", "output", "cpu"),
+    ]) as (c, y, z):
+        with ir.For("i", 0, 4, nid="L1") as i:
+            with ir.If(c[()] > 0):
+                with ir.For("j1", 0, 8, nid="L2a") as j:
+                    y[i, j] = i + j
+            with ir.For("j2", 0, 8, nid="L2b") as j:
+                z[i, j] = i * j
+    ast = ir.pop_ast()
+    print(ast)
+    s = ir.Schedule(ast)
+    s.fuse("L2a", "L2b")
+    ast = s.ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
+
+    with ir.VarDef([
+        ("c", (), "int32", "input", "cpu"),
+        ("y", (4, 8), "int32", "output", "cpu"),
+        ("z", (4, 8), "int32", "output", "cpu"),
+    ]) as (c, y, z):
+        with ir.For("i", 0, 4) as i:
+            with ir.For("j", 0, 8) as j:
+                with ir.If(c[()] > 0):
+                    y[i, j] = i + j
+                z[i, j] = i * j
+    std = ir.pop_ast()
+
+    assert std.match(ast)
