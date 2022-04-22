d594fbf9927159388751ec095a89d6c8a5b9bd51
Shizhi Tang <rd0x01@gmail.com>
2022-04-22 13:22:07 +0800

    Reuse analyze/all_uses for analyze/check_all_defined

--- include/analyze/check_all_defined.h
+++ include/analyze/check_all_defined.h
@@ -3,26 +3,9 @@
 
 #include <unordered_set>
 
-#include <visitor.h>
+#include <ast.h>
 
 namespace ir {
-
-class CheckAllDefined : public Visitor {
-    const std::unordered_set<std::string> &defs_;
-    bool allDef_ = true;
-
-  public:
-    CheckAllDefined(const std::unordered_set<std::string> &defs)
-        : defs_(defs) {}
-
-    bool allDef() const { return allDef_; }
-
-  protected:
-    void visit(const Var &op) override;
-    void visit(const Load &op) override;
-    void visit(const Store &op) override;
-    void visit(const ReduceTo &op) override;
-};
 
 bool checkAllDefined(const std::unordered_set<std::string> &defs,
                      const AST &op);
--- src/analyze/check_all_defined.cc
+++ src/analyze/check_all_defined.cc
@@ -1,44 +1,16 @@
+#include <analyze/all_uses.h>
 #include <analyze/check_all_defined.h>
 
 namespace ir {
 
-void CheckAllDefined::visit(const Var &op) {
-    if (!allDef_) {
-        return;
-    }
-    Visitor::visit(op);
-    allDef_ &= !!defs_.count(op->name_);
-}
-
-void CheckAllDefined::visit(const Load &op) {
-    if (!allDef_) {
-        return;
-    }
-    Visitor::visit(op);
-    allDef_ &= !!defs_.count(op->var_);
-}
-
-void CheckAllDefined::visit(const Store &op) {
-    if (!allDef_) {
-        return;
-    }
-    Visitor::visit(op);
-    allDef_ &= !!defs_.count(op->var_);
-}
-
-void CheckAllDefined::visit(const ReduceTo &op) {
-    if (!allDef_) {
-        return;
-    }
-    Visitor::visit(op);
-    allDef_ &= !!defs_.count(op->var_);
-}
-
 bool checkAllDefined(const std::unordered_set<std::string> &defs,
                      const AST &op) {
-    CheckAllDefined visitor(defs);
-    visitor(op);
-    return visitor.allDef();
+    for (auto &&name : allNames(op)) {
+        if (!defs.count(name)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 } // namespace ir
