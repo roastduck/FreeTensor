e8fe9b98699dabc1c6aae0c4dbce04b8626c5cc4
Shizhi Tang <rd0x01@gmail.com>
2022-04-24 22:00:08 +0800

    2.pass/test_shrink_for.py::test_linear_bounds

--- test/2.pass/test_shrink_for.py
+++ test/2.pass/test_shrink_for.py
@@ -61,3 +61,25 @@ def test_multiple_iterators():
     std = ir.pop_ast()
 
     assert std.match(ast)
+
+
+def test_linear_bounds():
+    with ir.VarDef([("x", (11, 21), "int32", "input", "cpu"),
+                    ("y", (11, 21), "int32", "output", "cpu")]) as (x, y):
+        with ir.For("i", 0, 10000) as i:
+            with ir.For("j", 0, 21) as j:
+                with ir.If(ir.l_and(i - j <= 0, i + j <= 20)):
+                    y[i, j] = x[i, j] * 2
+    ast = ir.pop_ast()
+    print(ast)
+    ast = ir.lower(ast)
+    print(ast)
+
+    with ir.VarDef([("x", (11, 21), "int32", "input", "cpu"),
+                    ("y", (11, 21), "int32", "output", "cpu")]) as (x, y):
+        with ir.For("i", 0, 11) as i:
+            with ir.For("j", ir.any(), ir.any()) as j:
+                y[i, j] = x[i, j] * 2
+    std = ir.pop_ast()
+
+    assert std.match(ast)
