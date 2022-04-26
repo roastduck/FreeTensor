4dcf8aaef1a519cb622058f3919ef8246ea44fd9
Blealtan <blealtan@outlook.com>
2022-04-26 16:48:13 +0800

    Fix function parameters: add nonlocals for them. (#65)
    * Fix function parameters: add nonlocals for them.
    
    * Fix nested function and for loops.

--- test/5.frontend/test_transformer_basic.py
+++ test/5.frontend/test_transformer_basic.py
@@ -145,6 +145,33 @@ def test_static_if():
     assert np.array_equal(y_func, y_std)
 
 
+def test_static_if_2():
+
+    flag = True
+
+    @ir.inline
+    def f(y, value):
+        for i in range(0, 4):
+            if flag:
+                value = 1
+            y[i] = value
+
+    @ir.transform
+    def test(y):
+        ir.declare_var(y, (4,), "int32", "output", "cpu")
+        f(y, 0)
+
+    func = ir.lower(test, ir.CPU())
+    code = ir.codegen(func, ir.CPU())
+    y_np = np.zeros((4,), dtype="int32")
+    y_arr = ir.Array(y_np, ir.Device(ir.CPU()))
+    ir.Driver(func, code, ir.Device(ir.CPU()))(y=y_arr)
+    y_np = y_arr.numpy()
+
+    y_std = np.array([1, 1, 1, 1], dtype="int32")
+    assert np.array_equal(y_np, y_std)
+
+
 def test_for_range():
 
     def test(x):
