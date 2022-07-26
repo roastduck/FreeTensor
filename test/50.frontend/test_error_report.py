44509089e648327de3d16a67671332103b690135
Blealtan Cao <blealtan@outlook.com>
2022-07-26 15:23:26 +0800

    Add test cases for the fixes.

--- test/50.frontend/test_error_report.py
+++ test/50.frontend/test_error_report.py
@@ -32,3 +32,66 @@ def test_variable_not_declared():
     assert f"File \"{file}\", line {line_foo}" in e.value.args[0]
     assert f"File \"{file}\", line {line_bar}" in e.value.args[0]
     assert f"File \"{file}\", line {line_ab}" in e.value.args[0]
+
+def test_empty_line():
+
+    @ft.inline
+    def bar(a, b):
+        a + c
+
+    @ft.inline
+    def foo(a, b):
+
+        bar(a, b)
+
+    with pytest.raises(ft.StagingError) as e:
+
+        @ft.transform
+        def test(a, b):
+            a: ft.Var[(1,), "int32", "input", "cpu"]
+            b: ft.Var[(1,), "int32", "input", "cpu"]
+            foo(a, b)
+
+    frame_info = inspect.getframeinfo(inspect.currentframe())
+    line_foo = frame_info.lineno - 2
+    line_bar = frame_info.lineno - 10
+    line_ab = frame_info.lineno - 15
+    file = frame_info.filename
+
+    print(e.value.args[0])
+    assert f"File \"{file}\", line {line_foo}" in e.value.args[0]
+    assert f"File \"{file}\", line {line_bar}" in e.value.args[0]
+    assert f"File \"{file}\", line {line_ab}" in e.value.args[0]
+
+
+def test_unusual_indent():
+
+    @ft.inline
+    def bar(a, b):
+        a + c
+
+    @ft.inline
+    def foo(a, b):
+        bar(a, b)
+        return '''Try to break the _remove_indent
+------------------
+'''
+
+    with pytest.raises(ft.StagingError) as e:
+
+        @ft.transform
+        def test(a, b):
+            a: ft.Var[(1,), "int32", "input", "cpu"]
+            b: ft.Var[(1,), "int32", "input", "cpu"]
+            foo(a, b)
+
+    frame_info = inspect.getframeinfo(inspect.currentframe())
+    line_foo = frame_info.lineno - 2
+    line_bar = frame_info.lineno - 13
+    line_ab = frame_info.lineno - 17
+    file = frame_info.filename
+
+    print(e.value.args[0])
+    assert f"File \"{file}\", line {line_foo}" in e.value.args[0]
+    assert f"File \"{file}\", line {line_bar}" in e.value.args[0]
+    assert f"File \"{file}\", line {line_ab}" in e.value.args[0]
