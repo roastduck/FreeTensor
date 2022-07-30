4fb6293eef9c6226c943630b7517ee21c0baa9ae
Blealtan Cao <blealtan@outlook.com>
2022-07-31 02:00:53 +0800

    Deployed 257ff6f9 with MkDocs version: 1.3.0

--- doxygen/html/search/variables_10.js
+++ doxygen/html/search/variables_10.js
@@ -3,13 +3,14 @@ var searchData=
   ['randvars_5f_0',['randVars_',['../classfreetensor_1_1RandCtxImpl.html#a8378717e9212b53d00c87be42509876a',1,'freetensor::RandCtxImpl']]],
   ['reads_1',['reads',['../structfreetensor_1_1ForsWithDataReuse.html#a869afaeab890fd3970f86482cc020f8e',1,'freetensor::ForsWithDataReuse::reads()'],['../structfreetensor_1_1ForWithStore.html#a734a45516251b6ee6719a1f67e17c64d',1,'freetensor::ForWithStore::reads()']]],
   ['readsitself_2',['readsItself',['../structfreetensor_1_1ForWithStore.html#ab225b5eea3e6b524ec04c9bbdc75745f',1,'freetensor::ForWithStore']]],
-  ['reductionloops_3',['reductionLoops',['../structfreetensor_1_1ForsWithDataReuse.html#a22230c261d600e01f315dede984b51c8',1,'freetensor::ForsWithDataReuse']]],
-  ['reductionlooptiling_4',['reductionLoopTiling',['../structfreetensor_1_1MultiLevelTilingAnnotation.html#acdd3e1524308428978e7a1b3418b11ff',1,'freetensor::MultiLevelTilingAnnotation']]],
-  ['reductionlooptimes_5f_5',['reductionLoopTimes_',['../classfreetensor_1_1MultiLevelTilingPart.html#acea5a4ddbe3fbaad59c32221cce45a08',1,'freetensor::MultiLevelTilingPart']]],
-  ['reductions_5f_6',['reductions_',['../structfreetensor_1_1ForProperty.html#aeecb09bc83d34f64242bd15a63a7984f',1,'freetensor::ForProperty']]],
-  ['result_5f_7',['result_',['../classfreetensor_1_1ScheduleLogItemImpl.html#a4ea2cf276644aa8223b3f4fd521e65d0',1,'freetensor::ScheduleLogItemImpl']]],
-  ['rettype_5f_8',['retType_',['../classfreetensor_1_1IntrinsicNode.html#aabdf2f79d915080781d4d4dfa53fa720',1,'freetensor::IntrinsicNode']]],
-  ['returnclosure_5f_9',['returnClosure_',['../structfreetensor_1_1FuncRet.html#a7fbbcc654f0d8b28041d3cbd46dca838',1,'freetensor::FuncRet']]],
-  ['returns_5f_10',['returns_',['../classfreetensor_1_1FuncNode.html#ad79b48ec957b8fbebce7c13725d2fc16',1,'freetensor::FuncNode']]],
-  ['rhs_5f_11',['rhs_',['../classfreetensor_1_1BinaryExprNode.html#ae87839d910b0d33dd0c7760ac14dc28a',1,'freetensor::BinaryExprNode']]]
+  ['realiter_5f_3',['realIter_',['../structfreetensor_1_1IterAxis.html#a70fa857717ca1f5235113f9efb6ce60c',1,'freetensor::IterAxis']]],
+  ['reductionloops_4',['reductionLoops',['../structfreetensor_1_1ForsWithDataReuse.html#a22230c261d600e01f315dede984b51c8',1,'freetensor::ForsWithDataReuse']]],
+  ['reductionlooptiling_5',['reductionLoopTiling',['../structfreetensor_1_1MultiLevelTilingAnnotation.html#acdd3e1524308428978e7a1b3418b11ff',1,'freetensor::MultiLevelTilingAnnotation']]],
+  ['reductionlooptimes_5f_6',['reductionLoopTimes_',['../classfreetensor_1_1MultiLevelTilingPart.html#acea5a4ddbe3fbaad59c32221cce45a08',1,'freetensor::MultiLevelTilingPart']]],
+  ['reductions_5f_7',['reductions_',['../structfreetensor_1_1ForProperty.html#aeecb09bc83d34f64242bd15a63a7984f',1,'freetensor::ForProperty']]],
+  ['result_5f_8',['result_',['../classfreetensor_1_1ScheduleLogItemImpl.html#a4ea2cf276644aa8223b3f4fd521e65d0',1,'freetensor::ScheduleLogItemImpl']]],
+  ['rettype_5f_9',['retType_',['../classfreetensor_1_1IntrinsicNode.html#aabdf2f79d915080781d4d4dfa53fa720',1,'freetensor::IntrinsicNode']]],
+  ['returnclosure_5f_10',['returnClosure_',['../structfreetensor_1_1FuncRet.html#a7fbbcc654f0d8b28041d3cbd46dca838',1,'freetensor::FuncRet']]],
+  ['returns_5f_11',['returns_',['../classfreetensor_1_1FuncNode.html#ad79b48ec957b8fbebce7c13725d2fc16',1,'freetensor::FuncNode']]],
+  ['rhs_5f_12',['rhs_',['../classfreetensor_1_1BinaryExprNode.html#ae87839d910b0d33dd0c7760ac14dc28a',1,'freetensor::BinaryExprNode']]]
 ];
