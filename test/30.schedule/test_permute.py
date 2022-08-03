e114fa9a4bebd21f35bafb45110cc9af54ee4973
Blealtan Cao <blealtan@outlook.com>
2022-08-03 19:32:08 +0800

    The check is correct for more cases.

--- test/30.schedule/test_permute.py
+++ test/30.schedule/test_permute.py
@@ -1,9 +1,12 @@
+import pytest
 import freetensor as ft
 
+
 def test_5_point_seidel():
+
     def schd(s: ft.Schedule):
         s.permute(['L1', 'L2'], lambda i, j: (i + j, j))
-    
+
     @ft.schedule(callback=schd)
     @ft.transform
     def test(x: ft.Var[(8, 8), 'float32', 'inout']):
@@ -12,3 +15,44 @@ def test_5_point_seidel():
             #! nid: L2
             for j in range(1, 7):
                 x[i, j] += x[i - 1, j] + x[i, j - 1] + x[i, j + 1] + x[i + 1, j]
+
+
+def test_9_point_seidel():
+
+    def schd(s: ft.Schedule):
+        s.permute(['L1', 'L2'], lambda i, j: (2 * i + j, j))
+
+    @ft.schedule(callback=schd)
+    @ft.transform
+    def test(x: ft.Var[(8, 8), 'float32', 'inout']):
+        #! nid: L1
+        for i in range(1, 7):
+            #! nid: L2
+            for j in range(1, 7):
+                delta = 0
+                for ii in [-1, 0, 1]:
+                    for jj in [-1, 0, 1]:
+                        if ii != 0 or jj != 0:
+                            delta += x[i + ii, j + jj]
+                x[i, j] += delta
+
+
+def test_9_point_seidel_failed():
+
+    def schd(s: ft.Schedule):
+        s.permute(['L1', 'L2'], lambda i, j: (i + j, j))
+
+    with pytest.raises(ft.InvalidSchedule):
+        @ft.schedule(callback=schd)
+        @ft.transform
+        def test(x: ft.Var[(8, 8), 'float32', 'inout']):
+            #! nid: L1
+            for i in range(1, 7):
+                #! nid: L2
+                for j in range(1, 7):
+                    delta = 0
+                    for ii in [-1, 0, 1]:
+                        for jj in [-1, 0, 1]:
+                            if ii != 0 or jj != 0:
+                                delta += x[i + ii, j + jj]
+                    x[i, j] += delta
