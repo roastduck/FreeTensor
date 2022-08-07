ee1bbadbdecd9f6f1a68f2a4b417c8116bf4a477
trrbivial <shenji233@gmail.com>
2022-08-08 05:07:43 +0800

    Add import

--- python/freetensor/core/serialize.py
+++ python/freetensor/core/serialize.py
@@ -1,2 +1,2 @@
-from freetensor_ffi import dump_ast
-from freetensor_ffi import load_ast
+from freetensor_ffi import dump_ast, dump_target, dump_device, dump_array
+from freetensor_ffi import load_ast, load_target, load_device, load_array
