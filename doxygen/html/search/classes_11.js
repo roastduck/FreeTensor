97b6d49cfe849774825b7749f05daaedd84149c3
Blealtan Cao <blealtan@outlook.com>
2022-09-05 15:29:40 +0800

    Deployed 90f9ee84 with MkDocs version: 1.3.0

--- doxygen/html/search/classes_11.js
+++ doxygen/html/search/classes_11.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['unaryexprnode_0',['UnaryExprNode',['../classfreetensor_1_1UnaryExprNode.html',1,'freetensor']]],
   ['undomakereduction_1',['UndoMakeReduction',['../classfreetensor_1_1UndoMakeReduction.html',1,'freetensor']]],
-  ['unrollpart_2',['UnrollPart',['../classfreetensor_1_1UnrollPart.html',1,'freetensor']]],
-  ['unrollrule_3',['UnrollRule',['../classfreetensor_1_1UnrollRule.html',1,'freetensor']]],
-  ['upperbound_4',['UpperBound',['../classfreetensor_1_1UpperBound.html',1,'freetensor']]],
-  ['usebuiltindiv_5',['UseBuiltinDiv',['../classfreetensor_1_1UseBuiltinDiv.html',1,'freetensor']]]
+  ['unexpectedqueryresult_2',['UnexpectedQueryResult',['../classfreetensor_1_1UnexpectedQueryResult.html',1,'freetensor']]],
+  ['unrollpart_3',['UnrollPart',['../classfreetensor_1_1UnrollPart.html',1,'freetensor']]],
+  ['unrollrule_4',['UnrollRule',['../classfreetensor_1_1UnrollRule.html',1,'freetensor']]],
+  ['upperbound_5',['UpperBound',['../classfreetensor_1_1UpperBound.html',1,'freetensor']]],
+  ['usebuiltindiv_6',['UseBuiltinDiv',['../classfreetensor_1_1UseBuiltinDiv.html',1,'freetensor']]]
 ];
