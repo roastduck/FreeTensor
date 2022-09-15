roastduck/FreeTensor

commit 1ae1658165bced85b4099a8545922f4d2fbc7b7f

[GPU] Fix when threadDim is an expression of iterators of loops outside the kernel (#225)

src/pass/gpu/normalize_threads.cc
@@ -133,6 +133,10 @@ bool CheckThreadNum::isLegalLen(const Expr &expr) {
 
 bool CheckThreadNum::isLegalLen(const std::unordered_set<std::string> &names) {
     for (auto &&name : names) {
+        if (hasLoop(name)) {
+            // Only iterators from outside of the kernel is OK
+            return !openLoopsInKernel_.count(loop(name));
+        }
         if (!hasDef(name) || buffer(name)->mtype() != MemType::ByValue) {
             return false;
         }
@@ -141,11 +145,16 @@ bool CheckThreadNum::isLegalLen(const std::unordered_set<std::string> &names) {
 }
 
 Stmt CheckThreadNum::visit(const For &_op) {
-    auto __op = BaseClass::visit(_op);
-    ASSERT(__op->nodeType() == ASTNodeType::For);
-    auto op = __op.as<ForNode>();
+    if (std::holds_alternative<CUDAScope>(_op->property_->parallel_)) {
+        openLoopsInKernel_.insert(_op);
 
-    if (op->property_->parallel_ != serialScope) {
+        auto oldInKernel = inKernel_;
+        inKernel_ = true;
+        auto __op = BaseClass::visit(_op);
+        ASSERT(__op->nodeType() == ASTNodeType::For);
+        auto op = __op.as<ForNode>();
+        inKernel_ = oldInKernel;
+
         if (!isLegalLen(op->begin_)) {
             op->body_ =
                 makeIf(makeGE(makeVar(op->iter_), op->begin_), op->body_);
@@ -160,9 +169,13 @@ Stmt CheckThreadNum::visit(const For &_op) {
                 }
             }
             if (!begin.isValid()) {
-                throw InvalidProgram("Length of " +
-                                     toString(op->property_->parallel_) +
-                                     " should have a finite bound");
+                throw InvalidProgram(
+                    "Length of " + toString(op->property_->parallel_) +
+                    " should have a finite bound. Note: if you are making a "
+                    "dynamic ranged threadIdx or blockIdx loop, please use "
+                    "memory type \"byvalue\" for its range, because it is used "
+                    "both for launching the kernel and guarding the execution "
+                    "inside the kernel");
             }
             op->begin_ = std::move(begin);
         }
@@ -178,18 +191,32 @@ Stmt CheckThreadNum::visit(const For &_op) {
                 }
             }
             if (!end.isValid()) {
-                throw InvalidProgram("Length of " +
-                                     toString(op->property_->parallel_) +
-                                     " should have a finite bound");
+                throw InvalidProgram(
+                    "Length of " + toString(op->property_->parallel_) +
+                    " should have a finite bound. Note: if you are making a "
+                    "dynamic ranged threadIdx or blockIdx loop, please use "
+                    "memory type \"byvalue\" for its range, because it is used "
+                    "both for launching the kernel and guarding the execution "
+                    "inside the kernel");
             }
             op->end_ = std::move(end);
         }
         ASSERT(op->step_->nodeType() == ASTNodeType::IntConst &&
                op->step_.as<IntConstNode>()->val_ == 1);
         op->len_ = makeSub(op->end_, op->begin_);
-    }
 
-    return op;
+        openLoopsInKernel_.erase(_op);
+        return op;
+    } else {
+        if (inKernel_) {
+            openLoopsInKernel_.insert(_op);
+            auto ret = BaseClass::visit(_op);
+            openLoopsInKernel_.erase(_op);
+            return ret;
+        } else {
+            return BaseClass::visit(_op);
+        }
+    }
 }
 
 Stmt normalizeThreads(const Stmt &_op) {

test/40.codegen/gpu/test_gpu.py
@@ -488,6 +488,32 @@ def test_use_cpu_iters():
     assert np.array_equal(y_np, y_std)
 
 
+def test_dynamic_thread_dim_by_cpu_iters():
+    with ft.VarDef([("n", (4,), "int32", "input", "byvalue"),
+                    ("y", (4, 20), "int32", "inout", "gpu/global")]) as (n, y):
+        with ft.For("i", 0, 4, label="Li") as i:
+            with ft.For("j", 0, n[i], label="Lj") as j:
+                y[i, j] = i * j
+
+    with device:
+        s = ft.Schedule(ft.Func("main", ["n", "y"], [], ft.pop_ast()))
+        s.parallelize('Lj', "threadIdx.x")
+        func = ft.lower(s.func(), verbose=1)
+        code = ft.codegen(func, verbose=True)
+        n_np = np.array([5, 10, 15, 20], dtype="int32")
+        y_np = np.zeros((4, 20), dtype="int32")
+        n_arr = ft.Array(n_np)
+        y_arr = ft.Array(y_np)
+        ft.build_binary(code)(n_arr, y_arr)
+        y_np = y_arr.numpy()
+
+    y_std = np.array([[i * j if j < n else 0
+                       for j in range(20)]
+                      for i, n in enumerate([5, 10, 15, 20])],
+                     dtype="int32")
+    assert np.array_equal(y_np, y_std)
+
+
 def test_intrinsic():
 
     @ft.transform

include/pass/gpu/normalize_threads.h
@@ -49,6 +49,8 @@ class CheckThreadNum : public CompTransientBounds<SymbolTable<Mutator>> {
     typedef CompTransientBounds<SymbolTable<Mutator>> BaseClass;
 
     CompUniqueBounds bound_;
+    std::unordered_set<For> openLoopsInKernel_;
+    bool inKernel_ = false;
 
   public:
     CheckThreadNum() : bound_(*this) {}

commit c2d4f95edb2e01679bd5bd8b89d2a19f67edfd4a

Fixed a bug introduced in #225

python/freetensor/core/passes.py
@@ -51,7 +51,7 @@ def lower(ast=None,
         Lower the AST to a target with target-specific passes, then the AST can
         be used for codegen. If not set, use the default Target in Config
     skip_passes : Sequence[str] (Optional)
-        Skip some pass for testing or debugging. Names in `skipPasses` are in
+        Skip some pass for testing or debugging. Names in `skip_passes` are in
         underscore_style, as in Python. Please note that some passes will not be
         skipped even specified in these parameter, because they are indirectly
         called in some other passes

src/pass/gpu/normalize_threads.cc
@@ -135,9 +135,10 @@ bool CheckThreadNum::isLegalLen(const std::unordered_set<std::string> &names) {
     for (auto &&name : names) {
         if (hasLoop(name)) {
             // Only iterators from outside of the kernel is OK
-            return !openLoopsInKernel_.count(loop(name));
-        }
-        if (!hasDef(name) || buffer(name)->mtype() != MemType::ByValue) {
+            if (openLoopsInKernel_.count(loop(name))) {
+                return false;
+            }
+        } else if (!hasDef(name) || buffer(name)->mtype() != MemType::ByValue) {
             return false;
         }
     }

test/40.codegen/gpu/test_gpu.py
@@ -439,7 +439,7 @@ def test_dynamic_2d_array():
     assert np.array_equal(y_np, y_std)
 
 
-def test_dynamic_thread_dim():
+def test_dynamic_thread_dim_1():
     with ft.VarDef("n", (), "int32", "input", "byvalue") as n:
         with ft.VarDef([
             ("x", (n, n), "int32", "input", "gpu/global"),
@@ -465,6 +465,50 @@ def test_dynamic_thread_dim():
 
     y_std = x_np + 1
     assert np.array_equal(y_np, y_std)
+
+
+def test_dynamic_thread_dim_2():
+
+    @ft.transform()
+    def func(x, offset, y):
+        x: ft.Var[(27, 256), "float32", "input", "gpu/global"]
+        offset: ft.Var[(28,), "int32", "input", "byvalue"]
+        y: ft.Var[(256,), "float32", "output", "gpu/global"]
+
+        #! label: Li
+        for i in range(27):
+            cur_start = offset[i]
+            cur_end = offset[i + 1]
+            #! label: La
+            for a in range(cur_start, cur_end):
+                #! label: Loc
+                for oc in range(256):
+                    y[oc] += x[i, oc]
+
+    s = ft.Schedule(func)
+    by, la = s.split("La", factor=4)
+    bx, loc = s.split("Loc", factor=4)
+    s.reorder([by, bx, la, loc])
+    s.parallelize(by, "blockIdx.y")
+    s.parallelize(bx, "blockIdx.x")
+    s.parallelize(la, "threadIdx.y")
+    s.parallelize(loc, "threadIdx.x")
+    func = s.func()
+    func = ft.lower(func, target, verbose=1)
+
+    with ft.VarDef([("x", (27, 256), "float32", "input", "gpu/global"),
+                    ("offset", (28,), "int32", "input", "byvalue"),
+                    ("y", (256,), "float32", "output", "gpu/global")
+                   ]) as (x, offset, y):
+        with ft.For("i", 0, 27) as i:
+            with ft.For("blockIdx.y", 0, ft.any()) as by:
+                with ft.For("blockIdx.x", 0, 64) as bx:
+                    with ft.For("threadIdx.y", 0,
+                                ft.min(offset[i + 1] + -1 * offset[i],
+                                       4)) as ty:
+                        with ft.If(ty < ft.any()):
+                            ft.Any()
+    assert ft.pop_ast().match(func.body)
 
 
 def test_use_cpu_iters():