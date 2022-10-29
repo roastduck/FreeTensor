5937572d50debece2cd538711dbc04ffd8fb665d
Blealtan Cao <blealtan@outlook.com>
2022-10-29 09:36:19 +0800

    Deployed 4981d1e4 with MkDocs version: 1.4.1

--- doxygen/html/search/related_4.js
+++ doxygen/html/search/related_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['makebuffer_0',['makeBuffer',['../classfreetensor_1_1Buffer.html#a8e0eec519e7b5296d7db52e439f58bec',1,'freetensor::Buffer']]],
-  ['maketensor_1',['makeTensor',['../classfreetensor_1_1Tensor.html#a753c13343946fe9200a1867911802eb8',1,'freetensor::Tensor::makeTensor()'],['../classfreetensor_1_1Tensor.html#a06a5c54d9fe1141c7bebbede1335cb16',1,'freetensor::Tensor::makeTensor()']]]
+  ['maketensor_1',['makeTensor',['../classfreetensor_1_1Tensor.html#a753c13343946fe9200a1867911802eb8',1,'freetensor::Tensor::makeTensor()'],['../classfreetensor_1_1Tensor.html#a06a5c54d9fe1141c7bebbede1335cb16',1,'freetensor::Tensor::makeTensor()']]],
+  ['max_2',['max',['../classfreetensor_1_1PBBuildExpr.html#a4c5280441b9e220d5af0bd533d737f6f',1,'freetensor::PBBuildExpr']]],
+  ['min_3',['min',['../classfreetensor_1_1PBBuildExpr.html#ae63019015fc051f7ac4917822f513d46',1,'freetensor::PBBuildExpr']]]
 ];
