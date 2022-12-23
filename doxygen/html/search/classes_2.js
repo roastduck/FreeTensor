85bcdd3750ae0942bf7dd95e059c8598f89f6708
Blealtan Cao <blealtan@outlook.com>
2022-12-23 19:42:27 +0800

    Deployed 48dcec3a with MkDocs version: 1.4.2

--- doxygen/html/search/classes_2.js
+++ doxygen/html/search/classes_2.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['backunroll_0',['BackUnroll',['../classfreetensor_1_1BackUnroll.html',1,'freetensor']]],
-  ['binaryexprnode_1',['BinaryExprNode',['../classfreetensor_1_1BinaryExprNode.html',1,'freetensor']]],
-  ['blendpass_2',['BlendPass',['../classfreetensor_1_1BlendPass.html',1,'freetensor']]],
-  ['boolconstnode_3',['BoolConstNode',['../classfreetensor_1_1BoolConstNode.html',1,'freetensor']]],
-  ['bothmetadataselector_4',['BothMetadataSelector',['../classfreetensor_1_1BothMetadataSelector.html',1,'freetensor']]],
-  ['bothselector_5',['BothSelector',['../classfreetensor_1_1BothSelector.html',1,'freetensor']]],
-  ['buffer_6',['Buffer',['../classfreetensor_1_1Buffer.html',1,'freetensor']]],
-  ['builtinsimplify_7',['BuiltinSimplify',['../classfreetensor_1_1BuiltinSimplify.html',1,'freetensor']]]
+  ['beforeselector_1',['BeforeSelector',['../classfreetensor_1_1BeforeSelector.html',1,'freetensor']]],
+  ['binaryexprnode_2',['BinaryExprNode',['../classfreetensor_1_1BinaryExprNode.html',1,'freetensor']]],
+  ['blendpass_3',['BlendPass',['../classfreetensor_1_1BlendPass.html',1,'freetensor']]],
+  ['boolconstnode_4',['BoolConstNode',['../classfreetensor_1_1BoolConstNode.html',1,'freetensor']]],
+  ['bothmetadataselector_5',['BothMetadataSelector',['../classfreetensor_1_1BothMetadataSelector.html',1,'freetensor']]],
+  ['bothselector_6',['BothSelector',['../classfreetensor_1_1BothSelector.html',1,'freetensor']]],
+  ['buffer_7',['Buffer',['../classfreetensor_1_1Buffer.html',1,'freetensor']]],
+  ['builtinsimplify_8',['BuiltinSimplify',['../classfreetensor_1_1BuiltinSimplify.html',1,'freetensor']]]
 ];
--- doxygen/html/search/classes_4.js
+++ doxygen/html/search/classes_4.js
@@ -5,9 +5,11 @@ var searchData=
   ['dependence_2',['Dependence',['../structfreetensor_1_1Dependence.html',1,'freetensor']]],
   ['descendantselector_3',['DescendantSelector',['../classfreetensor_1_1DescendantSelector.html',1,'freetensor']]],
   ['device_4',['Device',['../classfreetensor_1_1Device.html',1,'freetensor']]],
-  ['directcallerselector_5',['DirectCallerSelector',['../classfreetensor_1_1DirectCallerSelector.html',1,'freetensor']]],
-  ['discreteobservation_6',['DiscreteObservation',['../structfreetensor_1_1DiscreteObservation.html',1,'freetensor']]],
-  ['discreterandvar_7',['DiscreteRandVar',['../classfreetensor_1_1DiscreteRandVar.html',1,'freetensor']]],
-  ['driver_8',['Driver',['../classfreetensor_1_1Driver.html',1,'freetensor']]],
-  ['drivererror_9',['DriverError',['../classfreetensor_1_1DriverError.html',1,'freetensor']]]
+  ['directafterselector_5',['DirectAfterSelector',['../classfreetensor_1_1DirectAfterSelector.html',1,'freetensor']]],
+  ['directbeforeselector_6',['DirectBeforeSelector',['../classfreetensor_1_1DirectBeforeSelector.html',1,'freetensor']]],
+  ['directcallerselector_7',['DirectCallerSelector',['../classfreetensor_1_1DirectCallerSelector.html',1,'freetensor']]],
+  ['discreteobservation_8',['DiscreteObservation',['../structfreetensor_1_1DiscreteObservation.html',1,'freetensor']]],
+  ['discreterandvar_9',['DiscreteRandVar',['../classfreetensor_1_1DiscreteRandVar.html',1,'freetensor']]],
+  ['driver_10',['Driver',['../classfreetensor_1_1Driver.html',1,'freetensor']]],
+  ['drivererror_11',['DriverError',['../classfreetensor_1_1DriverError.html',1,'freetensor']]]
 ];
