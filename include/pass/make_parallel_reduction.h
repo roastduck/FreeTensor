0107cf4d1a2e212b66e85e8800ca0878b4bb1540
Shizhi Tang <rd0x01@gmail.com>
2023-01-10 09:23:28 +0800

    Allocate temporary buffer for atomic reduction in better place (#335)
    * Allocate temporary buffer for atomic reduction in better place
    
    * Minor fix

--- include/pass/make_parallel_reduction.h
+++ include/pass/make_parallel_reduction.h
@@ -7,6 +7,7 @@
 #include <analyze/comp_unique_bounds.h>
 #include <analyze/find_loop_variance.h>
 #include <analyze/symbol_table.h>
+#include <driver/target.h>
 #include <func.h>
 #include <mutator.h>
 #include <visitor.h>
@@ -48,6 +49,14 @@ class FindSerialLoopsOverReduce : public Visitor {
     void visit(const ReduceTo &op) override;
 };
 
+struct UseAtomicInfo {
+    // True if there is random access to the being-reduced variable. Note: even
+    // there is no random access, we may also forced to use atomic, for example
+    // cross-GPU-thread-block reduction. In this case, `isRandomAccess_` is
+    // false
+    bool isRandomAccess_;
+};
+
 /**
  * Lower supported parallel reductions to loop-carried reductions, which will be
  * further lowered to specific algorithms like binary reduction or local
@@ -67,11 +76,15 @@ class MakeLoopCarriedReduction
 
     CompUniqueBounds unique_;
 
-    std::unordered_map<ID, std::unordered_set<ID>>
-        &toAlter_; // ReduceTo ID -> Racing For ID. Supported reductions will be
-                   // deleted from this map, and the left ones will be passed to
-                   // `MakeAtomicReduction`
+    const std::unordered_map<ID, std::unordered_set<ID>>
+        &toAlter_; // ReduceTo ID -> Racing For ID
     const LoopVariExprMap &variantMap_;
+
+    // ReduceTo ID -> whether randomly accessed. For all reductions in
+    // `toAlter`, we first try to lower them as loop-carried reductions. If
+    // impossible, we then insert them to this map, which is passed to
+    // `MakeAtomicReduction`.
+    std::unordered_map<ID, UseAtomicInfo> toUseAtomic_;
 
     std::unordered_map<ID, ParallelScope> paraScopes_; // For Id -> parallel
     std::unordered_map<ID, std::vector<ReductionItemFactors>> forReductions_;
@@ -80,9 +93,11 @@ class MakeLoopCarriedReduction
 
   public:
     MakeLoopCarriedReduction(
-        std::unordered_map<ID, std::unordered_set<ID>> &toAlter,
+        const std::unordered_map<ID, std::unordered_set<ID>> &toAlter,
         const LoopVariExprMap &variantMap)
         : unique_(*this), toAlter_(toAlter), variantMap_(variantMap) {}
+
+    const auto &toUseAtomic() const { return toUseAtomic_; }
 
   protected:
     using BaseClass::visit;
@@ -97,28 +112,68 @@ class MakeLoopCarriedReduction
 class MakeAtomicReduction : public SymbolTable<Mutator> {
     typedef SymbolTable<Mutator> BaseClass;
 
-    const std::unordered_map<ID, std::unordered_set<ID>>
-        &toAlter_; // ReduceTo ID -> Racing For ID
+    const std::unordered_map<ID, UseAtomicInfo> &toUseAtomic_;
     const std::unordered_map<ID, std::vector<For>>
         &serialOverRed_; // ReduceTo ID -> [For], from inner to outer
     const LoopVariExprMap &variantMap_;
 
+    const Ref<Target> &target_;
+
     struct AtomicCacheInfo {
         ReduceTo oldNode_;
         std::vector<Expr> newShape_, newTargetIndices_;
+        bool isRandomAccess_;
         std::vector<bool> preserveDim_;
     };
     std::unordered_map<ID,
                        std::vector<AtomicCacheInfo>>
         cacheAtomic_; // loop ID -> [AtomicCacheInfo]
 
+    int64_t gpuThreadDim_ = 1;
+
+  private:
+    /**
+     * Check whether we should put a `shape`-shaped variable to local memory on
+     * a GPU
+     *
+     * Three types of shapes are rejected:
+     *
+     * 1. Large shapes, including:
+     *   a) Shapes larger than the register count. Even we put them to local
+     * memory, they will still land on DRAM. Since NVCC allocates stack frames
+     * (where our local memory variables go to,
+     * https://forums.developer.nvidia.com/t/out-of-memory-when-allocating-local-memory/238615)
+     * by maximum possible thread count, so it will be worse than using global
+     * memory.
+     *   b) Large shapes that cannot be held in local memory for the stack-frame
+     * reason above. Since we have no idea whether NVCC will put a local
+     * variable in local memory or registers, in case NVCC does put it in local
+     * memory, we don't want an OOM.
+     * 2. Shapes with dynamic dimensions. Since indirect accessing of registers
+     * is not supported, loops accessing a register array must be unrolled, but
+     * dynamic dimensions make them impossible to unroll. Even we put these
+     * variables to local memory, they will still land on DRAM. For the reason
+     * mentioned in Case 1a, we prefer global memory over local memory.
+     * 3. Variables being randomly accessed. Randomly access have to be
+     * implemented via indirect access, even we can unroll the loops. Just like
+     * Case 2, we put them to global memory.
+     *
+     * TODO: Move it to an architecture-specific pass
+     */
+    bool canResideInGPULocal(DataType dtype, const std::vector<Expr> &shape,
+                             bool isRandomAccess) const;
+
+    MemType localMType(MemType mtype, DataType dtype,
+                       const std::vector<Expr> &shape,
+                       bool isRandomAccess) const;
+
   public:
     MakeAtomicReduction(
-        const std::unordered_map<ID, std::unordered_set<ID>> &toAlter,
+        const std::unordered_map<ID, UseAtomicInfo> &toUseAtomic,
         const std::unordered_map<ID, std::vector<For>> &serialOverRed,
-        const LoopVariExprMap &variantMap)
-        : toAlter_(toAlter), serialOverRed_(serialOverRed),
-          variantMap_(variantMap) {}
+        const LoopVariExprMap &variantMap, const Ref<Target> &target)
+        : toUseAtomic_(toUseAtomic), serialOverRed_(serialOverRed),
+          variantMap_(variantMap), target_(target) {}
 
   protected:
     using BaseClass::visit;
@@ -132,8 +187,10 @@ class MakeAtomicReduction : public SymbolTable<Mutator> {
  * If all ReduceTo nodes in a parallel for all reduce into a loop-invariant
  * possition, we will use a race-free implementation. Otherwise, we will use
  * atomic operations.
+ *
+ * @param target : Target information. Can be null for target-agnostic debugging
  */
-Stmt makeParallelReduction(const Stmt &op);
+Stmt makeParallelReduction(const Stmt &op, const Ref<Target> &target);
 
 DEFINE_PASS_FOR_FUNC(makeParallelReduction)
 
