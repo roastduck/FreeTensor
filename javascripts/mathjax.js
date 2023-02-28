5fc476c0aed39d28a73cfc660e464fe34870f7b5
Blealtan Cao <blealtan@outlook.com>
2023-02-28 23:03:41 +0800

    Deployed 10a5b8fb with MkDocs version: 1.4.2

---
+++ javascripts/mathjax.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    tags: "ams"  // Equation numbering
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
