4f5eff1e537f6c010d56088f55a7c031d144204a
Blealtan Cao <blealtan@outlook.com>
2023-03-07 16:25:57 +0800

    Deployed 9471e7b8 with MkDocs version: 1.4.2

--- doxygen/html/search/enumvalues_9.js
+++ doxygen/html/search/enumvalues_9.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['land_0',['LAnd',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fa97491c07168f35f5ad8a87122e45d85c',1,'freetensor::LAnd()'],['../namespacefreetensor.html#aa6c382c26df3486888425f7cc54de5c3a97491c07168f35f5ad8a87122e45d85c',1,'freetensor::LAnd()']]],
   ['le_1',['LE',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915facfe6055d2e0503be378bb63449ec7ba6',1,'freetensor']]],
-  ['lnot_2',['LNot',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fafee411092b54a6d52ade2570028b7181',1,'freetensor']]],
-  ['load_3',['Load',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915faf19dbf2edb3a0bd74b0524d960ff21eb',1,'freetensor']]],
-  ['loadatversion_4',['LoadAtVersion',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fae41195428f448a7971141f894e236b2d',1,'freetensor']]],
-  ['lor_5',['LOr',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fae02dc30ef38a2f4cdaf9357ae6afcaa6',1,'freetensor::LOr()'],['../namespacefreetensor.html#aa6c382c26df3486888425f7cc54de5c3ae02dc30ef38a2f4cdaf9357ae6afcaa6',1,'freetensor::LOr()']]],
-  ['lt_6',['LT',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fac562607189d77eb9dfb707464c1e7b0b',1,'freetensor']]]
+  ['ln_2',['Ln',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fa9acdbc05ab7c5f2b2576422f0400963a',1,'freetensor']]],
+  ['lnot_3',['LNot',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fafee411092b54a6d52ade2570028b7181',1,'freetensor']]],
+  ['load_4',['Load',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915faf19dbf2edb3a0bd74b0524d960ff21eb',1,'freetensor']]],
+  ['loadatversion_5',['LoadAtVersion',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fae41195428f448a7971141f894e236b2d',1,'freetensor']]],
+  ['lor_6',['LOr',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fae02dc30ef38a2f4cdaf9357ae6afcaa6',1,'freetensor::LOr()'],['../namespacefreetensor.html#aa6c382c26df3486888425f7cc54de5c3ae02dc30ef38a2f4cdaf9357ae6afcaa6',1,'freetensor::LOr()']]],
+  ['lt_7',['LT',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fac562607189d77eb9dfb707464c1e7b0b',1,'freetensor']]]
 ];
--- doxygen/html/search/typedefs_8.js
+++ doxygen/html/search/typedefs_8.js
@@ -2,14 +2,15 @@ var searchData=
 [
   ['land_0',['LAnd',['../namespacefreetensor.html#a29af9180b2174d1decbd6dadcc8351d9',1,'freetensor']]],
   ['le_1',['LE',['../namespacefreetensor.html#ae9f93cc90f42d651b347a5bcf93da34d',1,'freetensor']]],
-  ['lnot_2',['LNot',['../namespacefreetensor.html#a6990fed8f22aa9ed6066f7666f19b4d8',1,'freetensor']]],
-  ['load_3',['Load',['../namespacefreetensor.html#a3d4f48b01603fa2e1e2733b8bd18f29b',1,'freetensor']]],
-  ['loadatversion_4',['LoadAtVersion',['../namespacefreetensor.html#a8e301f263abe440f3f2cb3831501720f',1,'freetensor']]],
-  ['loopvariexprmap_5',['LoopVariExprMap',['../namespacefreetensor.html#a2158112a82b5397919cff3bd1291f0d3',1,'freetensor']]],
-  ['loopvaritransvarmap_6',['LoopVariTransVarMap',['../namespacefreetensor.html#a370ade5be645f87853c1e456ac2b6b6a',1,'freetensor']]],
-  ['loopvariuniqvarmap_7',['LoopVariUniqVarMap',['../namespacefreetensor.html#a08c5fd132603c854d45098916c635dfb',1,'freetensor']]],
-  ['lor_8',['LOr',['../namespacefreetensor.html#ae31a832ffce68eae2b41332fecaec1bd',1,'freetensor']]],
-  ['lowerboundslist_9',['LowerBoundsList',['../classfreetensor_1_1CompUniqueBounds.html#a910b8cc3aa5a4910fbf2b843834856a3',1,'freetensor::CompUniqueBounds']]],
-  ['lowerboundsmap_10',['LowerBoundsMap',['../classfreetensor_1_1CompUniqueBounds.html#a8e342bfda4b0016a0696ecee4e369615',1,'freetensor::CompUniqueBounds']]],
-  ['lt_11',['LT',['../namespacefreetensor.html#a80f19ec8092d385ca68d575308c294bd',1,'freetensor']]]
+  ['ln_2',['Ln',['../namespacefreetensor.html#afd2c8a8e65aaca7d9216838255d375cf',1,'freetensor']]],
+  ['lnot_3',['LNot',['../namespacefreetensor.html#a6990fed8f22aa9ed6066f7666f19b4d8',1,'freetensor']]],
+  ['load_4',['Load',['../namespacefreetensor.html#a3d4f48b01603fa2e1e2733b8bd18f29b',1,'freetensor']]],
+  ['loadatversion_5',['LoadAtVersion',['../namespacefreetensor.html#a8e301f263abe440f3f2cb3831501720f',1,'freetensor']]],
+  ['loopvariexprmap_6',['LoopVariExprMap',['../namespacefreetensor.html#a2158112a82b5397919cff3bd1291f0d3',1,'freetensor']]],
+  ['loopvaritransvarmap_7',['LoopVariTransVarMap',['../namespacefreetensor.html#a370ade5be645f87853c1e456ac2b6b6a',1,'freetensor']]],
+  ['loopvariuniqvarmap_8',['LoopVariUniqVarMap',['../namespacefreetensor.html#a08c5fd132603c854d45098916c635dfb',1,'freetensor']]],
+  ['lor_9',['LOr',['../namespacefreetensor.html#ae31a832ffce68eae2b41332fecaec1bd',1,'freetensor']]],
+  ['lowerboundslist_10',['LowerBoundsList',['../classfreetensor_1_1CompUniqueBounds.html#a910b8cc3aa5a4910fbf2b843834856a3',1,'freetensor::CompUniqueBounds']]],
+  ['lowerboundsmap_11',['LowerBoundsMap',['../classfreetensor_1_1CompUniqueBounds.html#a8e342bfda4b0016a0696ecee4e369615',1,'freetensor::CompUniqueBounds']]],
+  ['lt_12',['LT',['../namespacefreetensor.html#a80f19ec8092d385ca68d575308c294bd',1,'freetensor']]]
 ];
