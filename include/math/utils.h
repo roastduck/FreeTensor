4e5cdbc0ea0c471b08d2d6a1a237c4225017844b
Shizhi Tang <rd0x01@gmail.com>
2023-03-18 17:16:27 +0800

    Fix function overload in pass/const_fold

--- include/math/utils.h
+++ include/math/utils.h
@@ -8,22 +8,28 @@
 
 namespace freetensor {
 
+template <typename T>
+concept IntegralExceptBool = requires {
+                                 requires std::integral<T>;
+                                 requires !std::same_as<T, bool>;
+                             };
+
 // NOTE: For floating-points, we always use double to deal with compile-time
 // operations
 
-inline auto floorDiv(std::integral auto a, std::integral auto b) {
+inline auto floorDiv(IntegralExceptBool auto a, IntegralExceptBool auto b) {
     auto res = a / b;
     auto rem = a % b;
     return res - (rem != 0 && ((rem < 0) != (b < 0)));
 }
 
-inline auto ceilDiv(std::integral auto a, std::integral auto b) {
+inline auto ceilDiv(IntegralExceptBool auto a, IntegralExceptBool auto b) {
     auto res = a / b;
     auto rem = a % b;
     return res + (rem != 0 && ((rem < 0) == (b < 0)));
 }
 
-inline auto mod(std::integral auto a, std::integral auto b) {
+inline auto mod(IntegralExceptBool auto a, IntegralExceptBool auto b) {
     auto m = a % b;
     if (m < 0) {
         // m += (b < 0) ? -b : b; // avoid this form: it is UB when b == INT_MIN
@@ -32,7 +38,7 @@ inline auto mod(std::integral auto a, std::integral auto b) {
     return m;
 }
 
-template <std::integral T, std::integral U> auto gcd(T _x, U _y) {
+template <IntegralExceptBool T, IntegralExceptBool U> auto gcd(T _x, U _y) {
     std::common_type_t<T, U> x = std::abs(_x), y = std::abs(_y);
     if (x < y) {
         std::swap(x, y);
@@ -45,7 +51,7 @@ template <std::integral T, std::integral U> auto gcd(T _x, U _y) {
     return x;
 }
 
-inline auto lcm(std::integral auto x, std::integral auto y) {
+inline auto lcm(IntegralExceptBool auto x, IntegralExceptBool auto y) {
     return x / gcd(x, y) * y;
 }
 
