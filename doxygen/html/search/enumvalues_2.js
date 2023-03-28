8922e420803c619faf3acd7ff58e870370030e91
Blealtan Cao <blealtan@outlook.com>
2023-03-28 18:21:42 +0800

    Deployed 723233bd with MkDocs version: 1.4.2

--- doxygen/html/search/enumvalues_2.js
+++ doxygen/html/search/enumvalues_2.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['cache_0',['Cache',['../namespacefreetensor.html#a923130db6aad12a2b5d46dfe001c08cbaab0cf104f39708eabd07b8cb67e149ba',1,'freetensor::Cache()'],['../namespacefreetensor.html#ac28eb3e0e6e276ccd7ffbe307afdaae8aab0cf104f39708eabd07b8cb67e149ba',1,'freetensor::Cache()']]],
+  ['cache_0',['Cache',['../namespacefreetensor.html#ac28eb3e0e6e276ccd7ffbe307afdaae8aab0cf104f39708eabd07b8cb67e149ba',1,'freetensor::Cache()'],['../namespacefreetensor.html#a923130db6aad12a2b5d46dfe001c08cbaab0cf104f39708eabd07b8cb67e149ba',1,'freetensor::Cache()']]],
   ['cachereduction_1',['CacheReduction',['../namespacefreetensor.html#ac28eb3e0e6e276ccd7ffbe307afdaae8aeb433320c0861d947ac42cec56354cea',1,'freetensor']]],
   ['cast_2',['Cast',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fa4cd9f3996d60790cd11c04f842ebc43c',1,'freetensor']]],
   ['ceil_3',['Ceil',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fab95a01ffffa8fcdd2a9af961937c097c',1,'freetensor']]],
   ['ceildiv_4',['CeilDiv',['../namespacefreetensor.html#afc87eee71f05eb84c5799d7f3492915fa30c212eb3184a5ed41f01a25c8124e8a',1,'freetensor']]],
-  ['cpu_5',['CPU',['../namespacefreetensor.html#af577e754b8b16f492893f80ae9d2b150a2b55387dd066c5bac646ac61543d152d',1,'freetensor::CPU()'],['../namespacefreetensor.html#a000c2c313211c04f40116e7f8117d66ba2b55387dd066c5bac646ac61543d152d',1,'freetensor::CPU()']]],
+  ['cpu_5',['CPU',['../namespacefreetensor.html#a000c2c313211c04f40116e7f8117d66ba2b55387dd066c5bac646ac61543d152d',1,'freetensor::CPU()'],['../namespacefreetensor.html#af577e754b8b16f492893f80ae9d2b150a2b55387dd066c5bac646ac61543d152d',1,'freetensor::CPU()']]],
   ['cpuheap_6',['CPUHeap',['../namespacefreetensor.html#af577e754b8b16f492893f80ae9d2b150a7245cc5f5bb9ad69fd022fb9bfb75436',1,'freetensor']]],
   ['custom_7',['Custom',['../namespacefreetensor.html#af346f9de09dc96bd110e29ff9af378c3a90589c47f06eb971d548591f23c285af',1,'freetensor']]]
 ];
