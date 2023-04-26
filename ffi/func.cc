roastduck/FreeTensor

commit c59109b6a14c05bacda236055d4bd02cc7a94d48

Enable inlined_invoke a function generated by AD (#389)
* Enable inlined_invoke a function generated by AD

* Format using yapf 0.33.0

ffi/func.cc
@@ -7,11 +7,49 @@ using namespace pybind11::literals;
 
 void init_ffi_ast_func(py::module_ &m) {
     py::class_<FuncParam>(m, "FuncParam")
+        .def(py::init<FuncParam>())
+        .def(py::init([](const std::string &name, const Ref<Array> &closure,
+                         bool updateClosure) {
+                 if (closure.isValid()) {
+                     // Because we load Array back to C++ part, we cannot keep
+                     // tracking user data with py::keep_alive
+                     closure->makePrivateCopy();
+                 }
+                 return FuncParam{name,
+                                  closure.isValid()
+                                      ? Ref<Ref<Array>>::make(closure)
+                                      : nullptr,
+                                  updateClosure};
+             }),
+             "name"_a, "closure"_a = nullptr, "update_closure"_a = false)
         .def_readonly("name", &FuncParam::name_)
         .def_readonly("update_closure", &FuncParam::updateClosure_)
         .def_property_readonly("is_in_closure", &FuncParam::isInClosure);
+    py::implicitly_convertible<std::string, FuncParam>();
 
+    auto initFuncRet = [](const std::string &name, DataType dtype,
+                          const Ref<Array> &closure, bool returnClosure) {
+        if (closure.isValid()) {
+            // Because we load Array back to C++ part, we cannot keep
+            // tracking user data with py::keep_alive
+            closure->makePrivateCopy();
+        }
+        return FuncRet{name, dtype,
+                       closure.isValid() ? Ref<Ref<Array>>::make(closure)
+                                         : nullptr,
+                       returnClosure};
+    };
     py::class_<FuncRet>(m, "FuncRet")
+        .def(py::init<FuncRet>())
+        .def(py::init(initFuncRet), "name"_a, "dtype"_a, "closure"_a = nullptr,
+             "return_closure"_a = false)
+        .def(py::init([&](const std::string &name, const std::string &dtype,
+                          const Ref<Array> &closure, bool returnClosure) {
+                 return initFuncRet(name, parseDType(dtype), closure,
+                                    returnClosure);
+             }),
+             "name"_a, "dtype"_a, "closure"_a = nullptr,
+             "return_closure"_a = false)
         .def_readonly("name", &FuncRet::name_)
         .def_readonly("dtype", &FuncRet::dtype_)
         .def_readonly("return_closure", &FuncRet::returnClosure_)
@@ -19,53 +57,30 @@ void init_ffi_ast_func(py::module_ &m) {
 
     m.attr("Func")
         .cast<py::class_<FuncNode, Func>>()
-        .def(py::init(
-                 static_cast<Func (*)(
-                     const std::string &, const std::vector<FuncParam> &,
-                     const std::vector<FuncRet> &, const Stmt &)>(&_makeFunc)),
-             "name"_a, "params"_a, "returns"_a, "body"_a)
-        .def(
-            py::init(
-                [](const std::string &name,
-                   const std::vector<std::string> &_params,
-                   const std::vector<std::pair<std::string, DataType>>
-                       &_returns,
-                   const Stmt &body,
-                   const std::unordered_map<std::string, Ref<Array>> &closure) {
-                    std::vector<FuncParam> params;
-                    std::vector<FuncRet> returns;
-                    params.reserve(_params.size());
-                    returns.reserve(_returns.size());
-                    for (auto &&p : _params) {
-                        if (closure.count(p)) {
-                            auto arr = closure.at(p);
-                            arr->makePrivateCopy(); // Because we load Array
-                                                    // back to C++ part, we
-                                                    // cannot keep tracking user
-                                                    // data with py::keep_alive
-                            params.emplace_back(p, Ref<Ref<Array>>::make(arr),
-                                                false);
-                        } else {
-                            params.emplace_back(p, nullptr, false);
-                        }
-                    }
-                    for (auto &&[p, dtype] : _returns) {
-                        if (closure.count(p)) {
-                            auto arr = closure.at(p);
-                            arr->makePrivateCopy(); // Because we load Array
-                                                    // back to C++ part, we
-                                                    // cannot keep tracking user
-                                                    // data with py::keep_alive
-                            returns.emplace_back(
-                                p, dtype, Ref<Ref<Array>>::make(arr), false);
-                        } else {
-                            returns.emplace_back(p, dtype, nullptr, false);
-                        }
-                    }
-                    return makeFunc(name, std::move(params), std::move(returns),
-                                    body);
-                }),
-            "name"_a, "params"_a, "returns"_a, "body"_a, "closure"_a)
+        .def(py::init([](const std::string &name,
+                         const std::vector<FuncParam> &_params,
+                         const std::vector<FuncRet> &_returns, const Stmt &body,
+                         const std::unordered_map<std::string, Ref<Array>>
+                             &extraClosure) {
+                 std::vector<FuncParam> params = _params;
+                 std::vector<FuncRet> returns = _returns;
+                 for (auto &p : params) {
+                     if (auto it = extraClosure.find(p.name_);
+                         it != extraClosure.end()) {
+                         p.closure_ = Ref<Ref<Array>>::make(it->second);
+                     }
+                 }
+                 for (auto &r : returns) {
+                     if (auto it = extraClosure.find(r.name_);
+                         it != extraClosure.end()) {
+                         r.closure_ = Ref<Ref<Array>>::make(it->second);
+                     }
+                 }
+                 return makeFunc(name, std::move(params), std::move(returns),
+                                 body);
+             }),
+             "name"_a, "params"_a, "returns"_a, "body"_a,
+             "extra_closure"_a = std::unordered_map<std::string, Ref<Array>>{})
         .def_readonly("name", &FuncNode::name_)
         .def_readonly("params", &FuncNode::params_)
         .def_readonly("returns", &FuncNode::returns_)

python/freetensor/core/__init__.py
@@ -8,8 +8,7 @@ from freetensor_ffi import (ASTNodeType, InvalidSchedule, InvalidAutoGrad,
 from .context import pop_ast, pop_ast_and_user_grads, StmtRange
 from .expr import *
 from .stmt import (VarDef, For, If, Else, Alloc, Free, Assert, MarkLabel,
-                   NamedScope, Invoke, Eval, Any, Func, MarkVersion,
-                   UserGradStaged)
+                   NamedScope, Invoke, Eval, Any, MarkVersion, UserGradStaged)
 from .analyze import *
 from .autograd import *
 from .passes import *
@@ -19,10 +18,12 @@ from .driver import *
 from .config import *
 from .serialize import *
 
-from .frontend import (transform, inline, empty, var, capture_var, Var,
-                       dynamic_range, static_range, push_for_backward, UserGrad)
+from .frontend import (empty, var, capture_var, Var, dynamic_range,
+                       static_range, push_for_backward, UserGrad)
 from .staging import (StagingError, StagedAssignable, StagedIterable,
                       StagedPredicate, StagedTypeAnnotation)
+from .transform import *
+from .func import *
 
 from .meta import *
 from .optimize import optimize, optimize_to_pytorch

python/freetensor/core/autograd.py
@@ -12,7 +12,8 @@ from freetensor_ffi import GradTapeMode
 from freetensor_ffi import output_all_intermediates
 
 from .analyze import find_stmt
-from .frontend import transform
+from .transform import transform
+from .func import Func
 
 
 class Return:
@@ -107,6 +108,11 @@ def _grad_func(impl,
         tapes = {find_stmt(func, t).id for t in tapes}
     fwd, bwd, req_map, prov_map = impl(func, req, prov, tapes, tape_in_closure,
                                        invert, user_grads)
+
+    # Wrap fwd and bwd (originally ft.ffi.Func with ft.Func)
+    fwd = Func(fwd.name, fwd.params, fwd.returns, fwd.body)
+    bwd = Func(bwd.name, bwd.params, bwd.returns, bwd.body)
+
     if verbose is not None and verbose >= 1:
         print("Forward pass from AD:", file=sys.stderr)
         print(fwd, file=sys.stderr)

python/freetensor/core/frontend.py
@@ -4,7 +4,6 @@ A frontend transforming user Python functions to ASTs via staging.
 
 import sys
 import numpy as np
-import functools
 import inspect
 from numbers import Number
 from typing import Callable, Dict, List, Sequence, Optional, Any, Union
@@ -12,15 +11,12 @@ from dataclasses import dataclass
 
 import freetensor_ffi as ffi
 
-from .context import pop_ast_and_user_grads
 from .expr import (dtype, mtype, ndim, l_and, l_or, l_not, if_then_else, shape,
                    VarVersionRef)
-from .stmt import (_VarDef, VarRef, For, If, Else, ctx_stack, Func, Assert,
-                   Invoke, MarkVersion, UserGradStaged)
+from .stmt import (_VarDef, VarRef, For, If, Else, ctx_stack, Assert, Invoke,
+                   MarkVersion, UserGradStaged)
 from .staging import (StagedPredicate, StagedTypeAnnotation, StagedAssignable,
-                      StagedIterable, StagingError, StagingOverload,
-                      TransformError)
-from .meta import add_outputting
+                      StagedIterable, StagingOverload)
 
 assert sys.version_info >= (3, 8), \
     "Python version lower than 3.8 is not supported"
@@ -29,7 +25,7 @@ assert sys.version_info >= (3, 8), \
 def staged_callable(staging, original, doc: Optional[str] = None):
 
     def impl(*args, **kwargs):
-        if _overload.in_staging():
+        if lang_overload.in_staging():
             return staging(*args, **kwargs)
         else:
             return original(*args, **kwargs)
@@ -53,14 +49,14 @@ class LifetimeScope:
         self.inner_scopes = []
 
     def __enter__(self):
-        _overload.lifetime_stack.append(self)
+        lang_overload.lifetime_stack.append(self)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         for scope in reversed(self.inner_scopes):
             scope.__exit__(exc_type, exc_val, exc_tb)
-        popped = _overload.lifetime_stack.pop()
+        popped = lang_overload.lifetime_stack.pop()
         if popped != self:
-            raise _overload.error(
+            raise lang_overload.error(
                 'LifetimeScope enter/exit not match, must be FILO')
 
     def register_inner_scope(self, scope):
@@ -189,7 +185,7 @@ class FreeTensorOverload(StagingOverload):
         ctx_stack.top().set_next_location(filename, lineno)
 
 
-_overload: FreeTensorOverload = FreeTensorOverload()
+lang_overload: FreeTensorOverload = FreeTensorOverload()
 
 
 def _register_as_predicate(ty):
@@ -205,7 +201,7 @@ def _register_as_predicate(ty):
 
     def _if_then_else_stmt(pred: ty, then_body: Callable[[], None],
                            else_body: Optional[Callable[[], None]]):
-        with _overload.allow_shortcut_scope(False):
+        with lang_overload.allow_shortcut_scope(False):
             with If(pred):
                 with LifetimeScope():
                     then_body()
@@ -222,7 +218,7 @@ def _register_as_predicate(ty):
         raise NotImplementedError()
 
     def _assert_stmt(pred: ty):
-        _overload.register_assert(pred)
+        lang_overload.register_assert(pred)
 
     StagedPredicate.register(ty)
     ty.logical_and = _logical_and
@@ -249,10 +245,10 @@ class VarCreator(StagedAssignable):
         '''Customized assign behavior. Creates a VarDef with its full name.'''
         if not self.assigned:
             self.assigned = True
-            return _overload.register_vardef(name, self.shape, self.dtype,
-                                             'cache', self.mtype)
+            return lang_overload.register_vardef(name, self.shape, self.dtype,
+                                                 'cache', self.mtype)
         else:
-            raise _overload.error(
+            raise lang_overload.error(
                 "Create new tensors in an `a = b = c`-like multi-assignment "
                 "is not supported")
 
@@ -349,11 +345,11 @@ def capture_var_fallback(arr: ffi.Array, name: str = 'captured'):
 
 
 def capture_var_staging(arr: ffi.Array, name: str = 'captured'):
-    return _overload.register_vardef(name,
-                                     arr.shape,
-                                     arr.dtype,
-                                     'input',
-                                     capture=arr)
+    return lang_overload.register_vardef(name,
+                                         arr.shape,
+                                         arr.dtype,
+                                         'input',
+                                         capture=arr)
 
 
 capture_var = staged_callable(capture_var_staging, capture_var_fallback,
@@ -385,8 +381,8 @@ class Var(StagedTypeAnnotation):
         self.shape, self.dtype, self.atype, self.mtype = shape, dtype, atype, mtype
 
     def annotate(self, name: str) -> VarRef:
-        return _overload.register_vardef(name, self.shape, self.dtype,
-                                         self.atype, self.mtype)
+        return lang_overload.register_vardef(name, self.shape, self.dtype,
+                                             self.atype, self.mtype)
 
 
 @dataclass
@@ -398,13 +394,13 @@ class VersionMarker(StagedAssignable):
         '''Customized assign behavior. Creates a MarkVersion with its full name.'''
         if not self.assigned:
             self.assigned = True
-            full_tape_name = _overload.fullname(tape_name)
+            full_tape_name = lang_overload.fullname(tape_name)
             MarkVersion(full_tape_name, self.var)
             return VarVersionRef(full_tape_name, self.var.full_shape,
                                  self.var.dtype, self.var.mtype,
                                  self.var.indices)
         else:
-            raise _overload.error(
+            raise lang_overload.error(
                 "Marking version in an `a = b = c`-like multi-assignment is not"
                 " supported")
 
@@ -497,7 +493,7 @@ class dynamic_range(StagedIterable):
     def foreach(self, name, body: Callable[[Any], None]) -> None:
         '''Customized foreach behavior. Creates a For loop.'''
         if not isinstance(name, str):
-            raise _overload.error(
+            raise lang_overload.error(
                 'dynamic_range only supports exactly one target variable')
 
         # Early optimizations
@@ -510,170 +506,11 @@ class dynamic_range(StagedIterable):
                     body(self.start)
                 return
 
-        with _overload.allow_shortcut_scope(False):
-            with For(_overload.fullname(name), self.start, self.stop,
+        with lang_overload.allow_shortcut_scope(False):
+            with For(lang_overload.fullname(name), self.start, self.stop,
                      self.step) as iter_var:
                 with LifetimeScope():
                     body(iter_var)
 
 
 static_range = range
-
-
-def _prepare_extra_locals(default_dynamic_range):
-    extra_locals = {'__ft__': sys.modules['freetensor']}
-    if default_dynamic_range:
-        extra_locals['range'] = dynamic_range
-    return extra_locals
-
-
-def transform(func=None, default_dynamic_range=True, verbose: int = 0):
-    '''
-    Transform a user function to an AST
-
-    Parameters
-    ----------
-    func : Python function
-        The user function to transform. If not specified, a partial function will
-        be returend, which can be used as a decorator
-    default_dynamic_range : bool
-        If True, the built-in range is replaced with freetensor.dynamic_range.
-        Defaults to True
-    verbose : int
-        0 = print nothing. 1 = print the resulting AST. 2 = 1 + print the generated
-        Python code that is used for transforming
-    '''
-
-    if func is None:
-        return functools.partial(transform,
-                                 default_dynamic_range=default_dynamic_range,
-                                 verbose=verbose)
-
-    if verbose is None:
-        verbose = 0
-
-    extra_locals = _prepare_extra_locals(default_dynamic_range)
-
-    params = list(inspect.signature(func).parameters)
-
-    staging_func = _overload.into_staging(func,
-                                          extra_locals,
-                                          verbose=verbose >= 2)
-
-    try:
-        # Initialize _overload to prepare for staging.
-        _overload.__init__()
-        # Create a new scope for the function
-        with LifetimeScope():
-            # Run staging function with the tensor program arguments' names as parameters
-            returns = staging_func(*params,
-                                   __freetensor_transform_outermost__=True)
-            # Check returned vardefs (if any)
-            if isinstance(returns, VarRef):
-                returns = [returns]
-            elif isinstance(returns, tuple):
-                for ret in returns:
-                    if not isinstance(ret, VarRef):
-                        raise _overload.error(
-                            'Illegal return at top level, need to be a `VarRef` or a tuple of `VarRef`s'
-                        )
-                returns = list(returns)
-            elif returns is None:
-                returns = []
-            else:
-                raise _overload.error(
-                    'Illegal return at top level, need to be a `VarRef` or a tuple of `VarRef`s'
-                )
-            # Set returned vardefs' access type to inout/output according to whether it was an input
-            for ret in returns:
-                ret.vardef.set_atype(add_outputting(ret.vardef.atype))
-            returns = [(ret.vardef.name, ret.vardef.dtype) for ret in returns]
-
-            # Set closure; they are from captured Arrays.
-            closure = _overload.closure
-    except StagingError:
-        raise
-    except TransformError:
-        raise
-    except Exception as e:
-        raise _overload.error('Exception occurred in staging') from e
-    finally:
-        # Despite whether the exception is raised, we need to clean up the ctx_stack
-        staged_ast, user_grads = pop_ast_and_user_grads()
-
-    staged = None
-
-    # Enable invoking a transformed AST in another function being transformed,
-    # via `inlined_invoke`
-    def prepare_inlined_invoke(*args, **kvs):
-        nonlocal staged
-        if _overload.in_staging():
-            if len(returns) == 1:
-                names = (func.__name__,)
-            else:
-                names = tuple(
-                    f"{func.__name__}.{i}" for i in range(len(returns)))
-            return _overload.register_inlined_invoke(names, staged, args, kvs)
-        else:
-            raise _overload.error(
-                'Unexpected call on a transformed AST. A transformed AST can only '
-                'be called in the following two ways: 1) called with actual data '
-                'after `@optimize`, and 2) called from another function to be '
-                '`@transform`ed')
-
-    staged = Func(func.__name__,
-                  params + list(closure.keys()),
-                  returns,
-                  staged_ast,
-                  closure,
-                  custom_callback=prepare_inlined_invoke,
-                  user_grads=user_grads)
-
-    if verbose >= 1:
-        print("The transformed AST is:", file=sys.stderr)
-        print(staged, file=sys.stderr)
-        print(file=sys.stderr)
-
-    return staged
-
-
-def inline(func=None,
-           src=None,
-           fallback=None,
-           default_dynamic_range=True,
-           verbose=False):
-    '''
-    Enable a user function to be called by a transformed function at run time
-
-    Parameters
-    ----------
-    func : Python function
-        The user function
-    src : str (Optional)
-        The source code of `func`. This parameter is only required if the source
-        code cannot be get automatically, e.g., if `func` is generated from a `exec`
-    default_dynamic_range : bool
-        If True, the built-in range is replaced with freetensor.dynamic_range.
-        Defaults to True
-    verbose : bool
-        True to print the generated Python code that is used for transforming
-    '''
-
-    if func is None:
-        return functools.partial(inline,
-                                 src=src,
-                                 fallback=fallback,
-                                 default_dynamic_range=default_dynamic_range,
-                                 verbose=verbose)
-
-    extra_locals = _prepare_extra_locals(default_dynamic_range)
-
-    # Do not initialize _overload here, since `into_staging` does not use the context.
-    # Keep the context as-is to support adding new inline functions during transforming.
-    # Such a case occurs when a transformed function dynamically imports a new inline.
-    transformed = _overload.into_staging(func,
-                                         extra_locals,
-                                         src,
-                                         verbose=verbose)
-
-    return functools.wraps(func)(staged_callable(transformed, fallback or func))

python/freetensor/core/func.py
@@ -1,0 +1,43 @@
+__all__ = ['FuncParam', 'FuncRet', 'Func']
+
+from typing import Sequence
+
+import freetensor_ffi as ffi
+from freetensor_ffi import FuncParam, FuncRet
+from .frontend import lang_overload
+
+
+class Func(ffi.Func):
+
+    def __init__(self,
+                 name: str,
+                 params: Sequence[FuncParam],
+                 returns: Sequence[FuncRet],
+                 body: ffi.Stmt,
+                 extra_closure={},
+                 user_grads=[]):
+        super().__init__(name, params, returns, body, extra_closure)
+        self.user_grads = user_grads
+
+        # Mimic a Python function
+        self.__name__ = name
+
+    def __call__(self, *args, **kvs):
+        '''
+        Enable invoking a transformed AST in another function being transformed, via
+        `inlined_invoke`
+        '''
+
+        if lang_overload.in_staging():
+            if len(self.returns) == 1:
+                names = (self.name,)
+            else:
+                names = tuple(
+                    f"{self.name}.{i}" for i in range(len(self.returns)))
+            return lang_overload.register_inlined_invoke(names, self, args, kvs)
+        else:
+            raise lang_overload.error(
+                'Unexpected call on a transformed AST. A transformed AST can only '
+                'be called in the following two ways: 1) called with actual data '
+                'after `@optimize`, and 2) called from another function to be '
+                '`@transform`ed')

python/freetensor/core/optimize.py
@@ -4,7 +4,8 @@ from typing import Optional, Callable, Union, Sequence
 import freetensor_ffi as ffi
 from freetensor_ffi import GradTapeMode
 
-from .frontend import transform, staged_callable
+from .frontend import staged_callable
+from .transform import transform
 from .autograd import grad
 from .schedule import Schedule, schedule
 from .passes import lower

python/freetensor/core/stmt.py
@@ -24,8 +24,8 @@ open_vardefs = {}
 def find_borrowed_vardefs(exprs: Sequence):
     borrowed_vardefs = set()
     for expr in exprs:
-        for name in ffi.all_reads(
-                expr if type(expr) is ffi.FrontendVarIdx else ffi.Expr(expr)):
+        for name in ffi.all_reads(expr if type(expr) is
+                                  ffi.FrontendVarIdx else ffi.Expr(expr)):
             borrowed_vardefs.add(open_vardefs[name])
     return borrowed_vardefs
 
@@ -481,27 +481,3 @@ class UserGradStaged:
         # Record the body to context
         ctx_stack.user_grads.append(
             ffi.StmtSetToUserGrad(self.ori_stmts, self.body))
-
-
-class Func(ffi.Func):
-
-    def __init__(self,
-                 name,
-                 params,
-                 returns,
-                 body,
-                 closure={},
-                 custom_callback=None,
-                 user_grads=[]):
-        super().__init__(
-            name, params,
-            list(map(lambda x: (x[0], ffi.DataType(x[1])), returns)), body,
-            closure)
-        self.custom_callback = custom_callback
-        self.user_grads = user_grads
-
-        # Mimic a Python function
-        self.__name__ = name
-
-    def __call__(self, *args, **kvs):
-        return self.custom_callback(*args, **kvs)

commit 8fedcb0b5c737198e527b584460c8b16949a6a7a

Fix copy broken by #389

ffi/func.cc
@@ -72,12 +72,18 @@ void init_ffi_ast_func(py::module_ &m) {
                  for (auto &p : params) {
                      if (auto it = extraClosure.find(p.name_);
                          it != extraClosure.end()) {
+                         // Because we load Array back to C++ part, we cannot
+                         // keep tracking user data with py::keep_alive
+                         it->second->makePrivateCopy();
                          p.closure_ = Ref<Ref<Array>>::make(it->second);
                      }
                  }
                  for (auto &r : returns) {
                      if (auto it = extraClosure.find(r.name_);
                          it != extraClosure.end()) {
+                         // Because we load Array back to C++ part, we cannot
+                         // keep tracking user data with py::keep_alive
+                         it->second->makePrivateCopy();
                          r.closure_ = Ref<Ref<Array>>::make(it->second);
                      }
                  }