195d6327158a0d1ac80bd4f62e5be303e0238ec5
Shizhi Tang <rd0x01@gmail.com>
2023-05-03 17:23:07 +0800

    Add libop.stack

--- python/freetensor/libop/concat.py
+++ python/freetensor/libop/concat.py
@@ -1,4 +1,4 @@
-__all__ = ['concat', 'concat_']
+__all__ = ['concat', 'concat_', 'stack', 'stack_']
 
 from typing import Sequence
 
@@ -30,6 +30,8 @@ def concat_(inputs: Sequence[core.VarRef], output: core.VarRef, axis: int = 0):
     off = 0
     for x in inputs:
         assert core.ndim(x) == core.ndim(inputs[0])
+        assert core.dtype(x) == core.dtype(inputs[0])
+        assert core.mtype(x) == core.mtype(inputs[0])
         output.select_slice(off, off + x.shape(axis), dim=axis)[...] = x
         off += x.shape(axis)
 
@@ -61,9 +63,60 @@ def concat(inputs: Sequence[core.VarRef], axis: int = 0):
     shape = inputs[0].shape()
     shape[axis] = 0
     for x in inputs:
-        assert core.dtype(x) == core.dtype(inputs[0])
-        assert core.mtype(x) == core.mtype(inputs[0])
         shape[axis] += x.shape(axis)
     output = core.empty(shape, core.dtype(inputs[0]), core.mtype(inputs[0]))
     concat_(inputs, output, axis)
+    return output
+
+
+@core.inline
+def stack_(inputs: Sequence[core.VarRef], output: core.VarRef, axis: int = 0):
+    '''
+    Concatenate a list of tensors into a single tensor on a new axis (in-place)
+
+    All input tensors must have the same shape, data type and memory type.
+
+    Parameters
+    ----------
+    inputs: Sequence[VarRef]
+        Tensors for concatenation
+    output: VarRef
+        Concatenation result
+    axis: int
+        Dimension number for concatenation. Negative axis means counting from the last
+        dimension
+    '''
+    axis = circular_axis(axis, core.ndim(inputs[0]) + 1)
+    for i, x in enumerate(inputs):
+        assert core.shape(x) == core.shape(inputs[0])
+        assert core.dtype(x) == core.dtype(inputs[0])
+        assert core.mtype(x) == core.mtype(inputs[0])
+        output.select(i, dim=axis)[...] = x
+
+
+@core.inline
+def stack(inputs: Sequence[core.VarRef], axis: int = 0):
+    '''
+    Concatenate a list of tensors into a single tensor on a new axis (out-of-place)
+
+    All input tensors must have the same shape, data type and memory type.
+
+    Parameters
+    ----------
+    inputs: Sequence[VarRef]
+        Tensors for concatenation
+    axis: int
+        Dimension number for concatenation. Negative axis means counting from the last
+        dimension
+
+    Returns
+    -------
+    VarRef
+        Concatenation result
+    '''
+    axis = circular_axis(axis, core.ndim(inputs[0]) + 1)
+    shape = inputs[0].shape()
+    shape.insert(axis, len(inputs))
+    output = core.empty(shape, core.dtype(inputs[0]), core.mtype(inputs[0]))
+    stack_(inputs, output, axis)
     return output
---
+++ test/60.libop/test_stack.py
@@ -0,0 +1,59 @@
+import freetensor as ft
+from freetensor import libop
+
+import torch
+import numpy as np
+
+
+def test_static():
+    device = ft.CPU()
+
+    @ft.optimize(device=device, verbose=1)
+    def f(x1, x2, x3, y):
+        x1: ft.Var[(3, 4), "float32", "input", "cpu"]
+        x2: ft.Var[(3, 4), "float32", "input", "cpu"]
+        x3: ft.Var[(3, 4), "float32", "input", "cpu"]
+        y: ft.Var[(3, 3, 4), "float32", "output", "cpu"]
+        #! label: stack
+        libop.stack_([x1, x2, x3], y, axis=1)
+
+    x1_torch = torch.rand(3, 4, dtype=torch.float32)
+    x1_arr = ft.Array(x1_torch.numpy())
+    x2_torch = torch.rand(3, 4, dtype=torch.float32)
+    x2_arr = ft.Array(x2_torch.numpy())
+    x3_torch = torch.rand(3, 4, dtype=torch.float32)
+    x3_arr = ft.Array(x3_torch.numpy())
+    y_torch = torch.zeros(3, 3, 4, dtype=torch.float32)
+    y_arr = ft.Array(y_torch.numpy())
+    f(x1_arr, x2_arr, x3_arr, y_arr)
+    y_torch = torch.tensor(y_arr.numpy())
+
+    assert torch.all(
+        torch.isclose(y_torch, torch.stack([x1_torch, x2_torch, x3_torch],
+                                           dim=1)))
+
+
+def test_out_of_place():
+    device = ft.CPU()
+
+    @ft.optimize(device=device, verbose=1)
+    def f(x1, x2, x3):
+        x1: ft.Var[(3, 4), "float32", "input", "cpu"]
+        x2: ft.Var[(3, 4), "float32", "input", "cpu"]
+        x3: ft.Var[(3, 4), "float32", "input", "cpu"]
+        #! label: stack
+        return libop.stack([x1, x2, x3], axis=1)
+
+    x1_torch = torch.rand(3, 4, dtype=torch.float32)
+    x1_arr = ft.Array(x1_torch.numpy())
+    x2_torch = torch.rand(3, 4, dtype=torch.float32)
+    x2_arr = ft.Array(x2_torch.numpy())
+    x3_torch = torch.rand(3, 4, dtype=torch.float32)
+    x3_arr = ft.Array(x3_torch.numpy())
+    y_arr = f(x1_arr, x2_arr, x3_arr)
+    y_torch = torch.tensor(y_arr.numpy())
+
+    assert np.array_equal(y_arr.shape, [3, 3, 4])
+    assert torch.all(
+        torch.isclose(y_torch, torch.stack([x1_torch, x2_torch, x3_torch],
+                                           dim=1)))
