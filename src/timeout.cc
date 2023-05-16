adb454591993a23f2684861ece3861c35574f81f
Shizhi Tang <rd0x01@gmail.com>
2023-05-16 16:00:42 +0800

    Fix unsafe I/O

--- src/timeout.cc
+++ src/timeout.cc
@@ -11,9 +11,23 @@
 
 namespace freetensor {
 
+static void writeUntilSuccess(int fd, const void *buf, size_t count) {
+    while (count > 0) {
+        auto thisCount = write(fd, buf, count);
+        if (thisCount < 0) {
+            ERROR("Error writing to a pipe");
+        }
+        count -= thisCount;
+        buf = ((const std::byte *)buf) + thisCount;
+    }
+}
+
 static void readUntilSucess(int fd, void *buf, size_t count) {
     while (count > 0) {
         auto thisCount = read(fd, buf, count);
+        if (thisCount < 0) {
+            ERROR("Error reading from a pipe");
+        }
         count -= thisCount;
         buf = ((std::byte *)buf) + thisCount;
     }
@@ -35,31 +49,39 @@ timeout(const std::function<std::vector<std::byte>()> &func, int seconds) {
         close(fd[0]);      // Close the read end of the pipe
         auto bytes = func();
         size_t size = bytes.size();
-        write(fd[1], &size, sizeof(size_t));
-        write(fd[1], bytes.data(), size);
+        try {
+            writeUntilSuccess(fd[1], &size, sizeof(size_t));
+            writeUntilSuccess(fd[1], bytes.data(), size);
+        } catch (...) {
+            close(fd[1]);
+            throw;
+        }
         close(fd[1]); // Close the write end of the pipe
         exit(EXIT_SUCCESS);
     } else {          // Parent process
         close(fd[1]); // Close the write end of the pipe
         std::vector<std::byte> result;
-        struct pollfd pfd;
-        pfd.fd = fd[0];
-        pfd.events = POLLIN;
-        int ret = poll(&pfd, 1,
-                       seconds * 1000); // Wait for the child process to finish
-                                        // or for the timeout to expire
-        if (ret == -1) {
+        try {
+            struct pollfd pfd;
+            pfd.fd = fd[0];
+            pfd.events = POLLIN;
+            // Wait for the child process to finish or for the timeout to expire
+            int ret = poll(&pfd, 1, seconds * 1000);
+            if (ret == -1) {
+                ERROR("poll returns an error");
+            } else if (ret == 0) { // Timeout expired, so kill the child process
+                WARNING("Function timed out after " + std::to_string(seconds) +
+                        " seconds");
+                kill(pid, SIGKILL);
+            } else { // Data is ready to be read from the pipe
+                size_t size;
+                readUntilSucess(fd[0], &size, sizeof(size_t));
+                result.resize(size);
+                readUntilSucess(fd[0], result.data(), size);
+            }
+        } catch (...) {
             close(fd[0]);
-            ERROR("poll returns an error");
-        } else if (ret == 0) { // Timeout expired, so kill the child process
-            WARNING("Function timed out after " + std::to_string(seconds) +
-                    " seconds");
-            kill(pid, SIGKILL);
-        } else { // Data is ready to be read from the pipe
-            size_t size;
-            readUntilSucess(fd[0], &size, sizeof(size_t));
-            result.resize(size);
-            readUntilSucess(fd[0], result.data(), size);
+            throw;
         }
         close(fd[0]); // Close the read end of the pipe
         return result;
