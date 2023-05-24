490619bbed8203dc65273b2b79fe035928b607df
Shizhi Tang <rd0x01@gmail.com>
2023-05-24 16:54:21 +0800

    Remove an unused import

--- python/freetensor/core/codegen.py
+++ python/freetensor/core/codegen.py
@@ -1,7 +1,6 @@
 from typing import Optional, Callable
 import sys
 import functools
-import astor
 from pygments import highlight
 from pygments.lexers import CppLexer, CudaLexer
 from pygments.formatters import TerminalFormatter
