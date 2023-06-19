70c342921397efe05b9e14d0f53a30a576c689c3
Shizhi Tang <rd0x01@gmail.com>
2023-06-19 20:13:35 +0800

    Improve error message from schedule/inline

--- include/schedule/inlining.h
+++ include/schedule/inlining.h
@@ -3,11 +3,14 @@
 
 #include <unordered_map>
 
+#include <analyze/track_stmt.h>
 #include <mutator.h>
 
 namespace freetensor {
 
-class MakeInline : public Mutator {
+class MakeInline : public TrackStmt<Mutator> {
+    typedef TrackStmt<Mutator> BaseClass;
+
     ID def_;
     std::string var_;
     const std::unordered_map<Load, Expr> &replace_;
@@ -17,6 +20,7 @@ class MakeInline : public Mutator {
         : def_(def), replace_(replace) {}
 
   protected:
+    using BaseClass::visit;
     Expr visit(const Load &op) override;
     Stmt visit(const Store &op) override;
     Stmt visit(const ReduceTo &op) override;
--- src/schedule/inlining.cc
+++ src/schedule/inlining.cc
@@ -17,10 +17,11 @@ Expr MakeInline::visit(const Load &op) {
         if (replace_.count(op)) {
             return (*this)(replace_.at(op));
         } else {
-            throw InvalidSchedule("Unable to inline into " + toString(op));
+            throw InvalidSchedule("Unable to inline into " + toString(op) +
+                                  " in " + toString(curStmt()));
         }
     } else {
-        return Mutator::visit(op);
+        return BaseClass::visit(op);
     }
 }
 
@@ -28,7 +29,7 @@ Stmt MakeInline::visit(const Store &op) {
     if (op->var_ == var_) {
         return makeStmtSeq({});
     } else {
-        return Mutator::visit(op);
+        return BaseClass::visit(op);
     }
 }
 
@@ -36,7 +37,7 @@ Stmt MakeInline::visit(const ReduceTo &op) {
     if (op->var_ == var_) {
         return makeStmtSeq({});
     } else {
-        return Mutator::visit(op);
+        return BaseClass::visit(op);
     }
 }
 
@@ -47,13 +48,13 @@ Stmt MakeInline::visit(const VarDef &_op) {
             throw InvalidSchedule("Cannot remove an I/O variable");
         }
         var_ = _op->name_;
-        auto __op = Mutator::visit(_op);
+        auto __op = BaseClass::visit(_op);
         ASSERT(__op->nodeType() == ASTNodeType::VarDef);
         auto op = __op.as<VarDefNode>();
         var_.clear();
         return op->body_;
     } else {
-        return Mutator::visit(_op);
+        return BaseClass::visit(_op);
     }
 }
 
