ffa3034760c1171a2fc1497fd9b68c349beecaaa
Shizhi Tang <rd0x01@gmail.com>
2023-06-19 17:09:25 +0800

    31.auto_schedule/test_auto_pluto.py::test_fission_then_pluto_fuse_2d_4way

--- src/schedule/auto_pluto.cc
+++ src/schedule/auto_pluto.cc
@@ -27,51 +27,73 @@ void Schedule::autoPluto(const Ref<Target> &target) {
 
     std::unordered_set<ID> tried;
     std::function<void(For nest)> tryLoop = [&, this](For nest) {
-        Stmt lastSplitter;
-        while (true) {
-            std::string pattern; // Select the nest splitter in this loop body
-            if (!lastSplitter.isValid()) {
-                pattern =
-                    "(<Store>|<ReduceTo>|<Eval>)<<-" + toString(nest->id());
-            } else {
-                pattern = "(<Store>|<ReduceTo>|<Eval>)&(<<-" +
-                          toString(nest->id()) + ")&(:>>" +
-                          toString(lastSplitter->id()) + ")";
-            }
-            Stmt splitter;
-            if (auto &&allNext = findAll(pattern); !allNext.empty()) {
-                splitter = allNext.front();
-            }
-            if (!splitter.isValid()) {
-                break;
+        auto triedWhenBegin = tried;
+        beginTransaction();
+
+        try {
+            std::vector<ID> parts;
+            Stmt lastSplitter;
+            while (true) {
+                std::string
+                    pattern; // Select the nest splitter in this loop body
+                if (!lastSplitter.isValid()) {
+                    pattern =
+                        "(<Store>|<ReduceTo>|<Eval>)<<-" + toString(nest->id());
+                } else {
+                    pattern = "(<Store>|<ReduceTo>|<Eval>)&(<<-" +
+                              toString(nest->id()) + ")&(:>>" +
+                              toString(lastSplitter->id()) + ")";
+                }
+                Stmt splitter;
+                if (auto &&allNext = findAll(pattern); !allNext.empty()) {
+                    splitter = allNext.front();
+                }
+                if (!splitter.isValid()) {
+                    break;
+                }
+
+                if (lastSplitter.isValid() && !tried.count(splitter->id())) {
+                    auto &&filterBefore =
+                        parseSelector("<<:" + toString(splitter->id()));
+                    auto &&filterAfter =
+                        parseSelector("!(<<:" + toString(splitter->id()) + ")");
+                    if (hasIntersect(allWrites(filterBefore, nest->body_),
+                                     allUses(filterAfter, nest->body_)) ||
+                        hasIntersect(allUses(filterBefore, nest->body_),
+                                     allWrites(filterAfter, nest->body_))) {
+                        try {
+                            auto &&[frontMap, backMap] = fission(
+                                nest->id(), FissionSide::Before, splitter->id(),
+                                false, "." + std::to_string(parts.size()), "");
+                            ID frontLoopId = frontMap.at(nest->id());
+                            ID backLoopId = backMap.at(nest->id());
+                            if (!parts.empty()) {
+                                parts.pop_back();
+                            }
+                            parts.emplace_back(frontLoopId);
+                            parts.emplace_back(backLoopId);
+                            tried.emplace(backMap.at(splitter->id()));
+                            nest = find(backLoopId).as<ForNode>();
+                        } catch (const InvalidSchedule &e) {
+                            // Do nothing
+                        }
+                    }
+                }
+
+                lastSplitter = splitter;
             }
 
-            if (lastSplitter.isValid() && !tried.count(splitter->id())) {
-                auto &&filterBefore =
-                    parseSelector("<<:" + toString(splitter->id()));
-                auto &&filterAfter =
-                    parseSelector("!(<<:" + toString(splitter->id()) + ")");
-                if (hasIntersect(allWrites(filterBefore, nest->body_),
-                                 allUses(filterAfter, nest->body_)) ||
-                    hasIntersect(allUses(filterBefore, nest->body_),
-                                 allWrites(filterAfter, nest->body_))) {
-                    beginTransaction();
-                    try {
-                        auto &&[frontMap, backMap] =
-                            fission(nest->id(), FissionSide::Before,
-                                    splitter->id(), false);
-                        ID frontLoopId = frontMap.at(nest->id());
-                        ID backLoopId = backMap.at(nest->id());
-                        plutoFuse(frontLoopId, backLoopId);
-                        commitTransaction();
-                        tried.emplace(backMap.at(splitter->id()));
-                    } catch (const InvalidSchedule &e) {
-                        abortTransaction();
-                    }
+            if (parts.size() > 1) {
+                ID fusedId = parts[0];
+                for (size_t i = 1, n = parts.size(); i < n; i++) {
+                    fusedId = plutoFuse(fusedId, parts[i]).first;
                 }
             }
 
-            lastSplitter = splitter;
+            commitTransaction();
+        } catch (const InvalidSchedule &e) {
+            abortTransaction();
+            tried = std::move(triedWhenBegin);
         }
 
         for (auto &&subNest :
--- test/31.auto_schedule/test_auto_pluto.py
+++ test/31.auto_schedule/test_auto_pluto.py
@@ -29,10 +29,9 @@ def test_fission_then_pluto_fuse():
     print(s.ast())
     logs = list(map(str, s.logs()))
     print(logs)
-    assert fnmatch_list(logs, [
-        'fission(Li, before, S1, *)',
-        'pluto_fuse($fission.0{Li}, $fission.1{Li}, *)'
-    ])
+    assert fnmatch_list(
+        logs,
+        ['fission(Li, before, S1, *)', 'pluto_fuse($fission.0{Li}, Li, *)'])
 
 
 def test_fission_then_pluto_fuse_2d():
@@ -53,7 +52,39 @@ def test_fission_then_pluto_fuse_2d():
     print(s.ast())
     logs = list(map(str, s.logs()))
     print(logs)
+    assert fnmatch_list(
+        logs,
+        ['fission(Li, before, S1, *)', 'pluto_fuse($fission.0{Li}, Li, *)'])
+
+
+def test_fission_then_pluto_fuse_2d_4way():
+    with ft.VarDef([("x1", (1000, 1000), "int32", "input", "cpu"),
+                    ("x2", (1000, 1000), "int32", "input", "cpu"),
+                    ("x3", (1000, 1000), "int32", "input", "cpu"),
+                    ("x4", (1000, 1000), "int32", "input", "cpu"),
+                    ("y", (1000, 1000), "int32", "inout", "cpu")
+                   ]) as (x1, x2, x3, x4, y):
+        with ft.For("i", 0, 999, label="Li") as i:
+            with ft.For("j", 0, 999, label="Lj") as j:
+                ft.MarkLabel("S0")
+                y[i, j] += x1[i, j]
+                ft.MarkLabel("S1")
+                y[i, j + 1] += x2[i, j]
+                ft.MarkLabel("S2")
+                y[i + 1, j] += x2[i, j]
+                ft.MarkLabel("S3")
+                y[i + 1, j + 1] += x2[i, j]
+
+    ast = ft.pop_ast(verbose=True)
+    s = ft.Schedule(ast)
+    s.auto_pluto(ft.CPU())
+    print(s.ast())
+    logs = list(map(str, s.logs()))
+    print(logs)
     assert fnmatch_list(logs, [
-        'fission(Li, before, S1, *)',
-        'pluto_fuse($fission.0{Li}, $fission.1{Li}, *)'
+        'fission(Li, before, S1, *)', 'fission(Li, before, S2, *)',
+        'fission(Li, before, S3, *)',
+        'pluto_fuse($fission.0{Li}, $fission.2{Li}, *)',
+        'pluto_fuse($pluto_fuse.fused.0{$fission.0{Li}, $fission.2{Li}}, $fission.3{Li}, *)',
+        'pluto_fuse($pluto_fuse.fused.0{$pluto_fuse.fused.0{$fission.0{Li}, $fission.2{Li}}, $fission.3{Li}}, Li, *)'
     ])
