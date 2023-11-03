e61ffc3673577d7c18f26467b4016843201e4cdd
Shizhi Tang <rd0x01@gmail.com>
2023-11-03 10:28:43 +0800

    Try not to touch unnecessary inner loops in schedule/auto_reorder

--- test/31.auto_schedule/test_auto_reorder.py
+++ test/31.auto_schedule/test_auto_reorder.py
@@ -34,3 +34,20 @@ def test_true_dep_is_more_important_than_reduction():
     logs = list(map(str, s.logs()))
     print(logs)
     assert logs == ["reorder(Lj, Li, move_out_imperfect)"]
+
+
+def test_no_reorder_inner_if_outer_has_enough_degree_of_parallelism():
+    with ft.VarDef([("x", (1000, 1000, 4), "int32", "input", "cpu"),
+                    ("y", (1000, 1000), "int32", "inout", "cpu")]) as (x, y):
+        with ft.For("i", 0, 1000, label="Li") as i:
+            with ft.For("j", 1, 1000, label="Lj") as j:
+                with ft.For("k", 1, 4, label="Lk") as k:
+                    y[i, k] += x[i, j, k]
+
+    ast = ft.pop_ast(verbose=True)
+    s = ft.Schedule(ast)
+    s.auto_reorder(ft.CPU())
+    print(s.ast())
+    logs = list(map(str, s.logs()))
+    print(logs)
+    assert logs == []
