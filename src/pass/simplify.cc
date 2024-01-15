7696f46594e18d4f1c2f2b8a4368036a720b757b
Shizhi Tang <rd0x01@gmail.com>
2024-01-15 19:55:43 +0800

    Remove redundant analysis in pass/pb_simplify (#584)
    * Remove redundant analysis in pass/pb_simplify
    
    * Fix broken test

--- src/pass/simplify.cc
+++ src/pass/simplify.cc
@@ -5,6 +5,7 @@
 #include <except.h>
 #include <math/min_max.h>
 #include <math/utils.h>
+#include <pass/annotate_conds.h>
 #include <pass/flatten_stmt_seq.h>
 #include <pass/replace_iter.h>
 #include <pass/simplify.h>
@@ -153,24 +154,35 @@ Stmt SimplifyPass::visitStmt(const Stmt &op) {
 }
 
 Expr SimplifyPass::visitExpr(const Expr &_op) {
-    auto op = BaseClass::visitExpr(_op);
-
-    // To avoid divergence
-    if (!HashComparator()(op, _op)) {
-        // E.g.
-        // (1) a[0 - 0] -> a[0]
-        // (2) (1 + 1) * a[0] -> 2 * a[0 - 0], because of the old bound
-        return op;
-    }
-
-    if (auto bound = unique_->getBound(op); bound.isValid()) {
-        Expr best = bound->simplestExpr(op, varScope_);
-        if (best.isValid() && !HashComparator()(best, op)) {
-            return best;
+    if (isInt(_op->dtype())) {
+        if (leafFirstBoundAnalysis_) {
+            auto op = BaseClass::visitExpr(_op);
+            if (!HashComparator()(op, _op)) {
+                // To avoid divergence
+                // E.g.
+                // (1) a[0 - 0] -> a[0]
+                // (2) (1 + 1) * a[0] -> 2 * a[0 - 0], because of the old bound
+                return op;
+            }
+            if (auto bound = unique_->getBound(op); bound.isValid()) {
+                Expr best = bound->simplestExpr(op, varScope_);
+                if (best.isValid() && !HashComparator()(best, op)) {
+                    return best;
+                }
+            }
+            return op;
+        } else {
+            if (auto bound = unique_->getBound(_op); bound.isValid()) {
+                Expr best = bound->simplestExpr(_op, varScope_);
+                if (best.isValid() && !HashComparator()(best, _op)) {
+                    return best;
+                }
+            }
+            return BaseClass::visitExpr(_op);
         }
-    }
 
-    if (_op->dtype() == DataType::Bool) {
+    } else if (isBool(_op->dtype())) {
+        auto op = BaseClass::visitExpr(_op);
         if (auto p = _op->parentExpr();
             !p.isValid() || p->dtype() != DataType::Bool) {
             // this is base bool expr
@@ -179,8 +191,11 @@ Expr SimplifyPass::visitExpr(const Expr &_op) {
                 op = normalized;
             }
         }
+        return op;
+
+    } else {
+        return BaseClass::visitExpr(_op);
     }
-    return op;
 }
 
 Expr SimplifyPass::visit(const Add &_op) {
@@ -711,6 +726,7 @@ Expr SimplifyPass::visit(const IfExpr &_op) {
     }
     ASSERT(__op->nodeType() == ASTNodeType::IfExpr);
     auto op = __op.as<IfExprNode>();
+
     if (op->cond_->nodeType() == ASTNodeType::BoolConst) {
         if (op->cond_.as<BoolConstNode>()->val_) {
             return op->thenCase_;
@@ -718,6 +734,79 @@ Expr SimplifyPass::visit(const IfExpr &_op) {
             return op->elseCase_;
         }
     }
+
+    if (HashComparator{}(op->thenCase_, op->elseCase_)) {
+        return op->thenCase_;
+    }
+
+    if (op->thenCase_->nodeType() == op->elseCase_->nodeType()) {
+        if (op->thenCase_->isUnary()) {
+            return makeUnary(
+                op->thenCase_->nodeType(),
+                makeIfExpr(op->cond_, op->thenCase_.as<UnaryExprNode>()->expr_,
+                           op->elseCase_.as<UnaryExprNode>()->expr_));
+        } else if (op->thenCase_->isBinary()) {
+            auto &&thenCase = op->thenCase_.as<BinaryExprNode>();
+            auto &&elseCase = op->elseCase_.as<BinaryExprNode>();
+            if (HashComparator{}(thenCase->lhs_, elseCase->lhs_)) {
+                return makeBinary(
+                    thenCase->nodeType(), thenCase->lhs_,
+                    makeIfExpr(op->cond_, thenCase->rhs_, elseCase->rhs_));
+            } else if (HashComparator{}(thenCase->rhs_, elseCase->rhs_)) {
+                return makeBinary(
+                    thenCase->nodeType(),
+                    makeIfExpr(op->cond_, thenCase->lhs_, elseCase->lhs_),
+                    thenCase->rhs_);
+            } else if (thenCase->isCommutative()) {
+                if (HashComparator{}(thenCase->lhs_, elseCase->rhs_)) {
+                    return makeBinary(
+                        thenCase->nodeType(), thenCase->lhs_,
+                        makeIfExpr(op->cond_, thenCase->rhs_, elseCase->lhs_));
+                } else if (HashComparator{}(thenCase->rhs_, elseCase->lhs_)) {
+                    return makeBinary(
+                        thenCase->nodeType(), thenCase->rhs_,
+                        makeIfExpr(op->cond_, thenCase->lhs_, elseCase->rhs_));
+                }
+            }
+        } else if (op->thenCase_->nodeType() == ASTNodeType::Cast) {
+            auto &&thenCase = op->thenCase_.as<CastNode>();
+            auto &&elseCase = op->elseCase_.as<CastNode>();
+            if (thenCase->destType_ == elseCase->destType_) {
+                return makeCast(
+                    makeIfExpr(op->cond_, thenCase->expr_, elseCase->expr_),
+                    thenCase->destType_);
+            }
+        } else if (op->thenCase_->nodeType() == ASTNodeType::Load) {
+            auto &&thenCase = op->thenCase_.as<LoadNode>();
+            auto &&elseCase = op->elseCase_.as<LoadNode>();
+            if (thenCase->var_ == elseCase->var_) {
+                // Since `var_` is the same, these must be the same
+                ASSERT(thenCase->indices_.size() == elseCase->indices_.size());
+                ASSERT(thenCase->loadType_ == elseCase->loadType_);
+                int diffCnt = 0;
+                std::vector<Expr> indices;
+                indices.reserve(thenCase->indices_.size());
+                for (auto &&[thenItem, elseItem] :
+                     views::zip(thenCase->indices_, elseCase->indices_)) {
+                    if (HashComparator{}(thenItem, elseItem)) {
+                        indices.emplace_back(thenItem);
+                    } else {
+                        diffCnt++;
+                        indices.emplace_back(
+                            makeIfExpr(op->cond_, thenItem, elseItem));
+                    }
+                }
+                if (diffCnt <= 1) {
+                    return makeLoad(thenCase->var_, std::move(indices),
+                                    thenCase->loadType_);
+                }
+            }
+        }
+        // TODO: We can also handle `Intrinsic`, but we must properly deal with
+        // `hasSideEffect_`, and check for data type in case of `... ?
+        // intrinsic(..., type_a) : intrinsic(..., type_b)`.
+    }
+
     return op;
 }
 
@@ -844,6 +933,24 @@ Stmt SimplifyPass::visit(const Assert &_op) {
         }
     }
     return op;
+}
+
+template <class Simplifier> static Stmt simplifyImpl(const Stmt &_op) {
+    auto op = _op;
+
+    for (int i = 0;; i++) {
+        auto newOp = annotateConds(op);
+        newOp = Simplifier()(newOp);
+        newOp = flattenStmtSeq(newOp);
+        if (HashComparator()(newOp, op) || i > 100) {
+            if (i > 100) {
+                WARNING("pass/simplify iterates over 100 rounds. Maybe there "
+                        "is a bug");
+            }
+            return newOp;
+        }
+        op = newOp;
+    }
 }
 
 Stmt builtinSimplify(const Stmt &op) {
